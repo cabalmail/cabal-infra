3bb98815e957d25f2ec1d8c5c06be5cb8227d262
Chris <chris_carr@jake.ccarr.com>
2021-03-13 11:32:33 -0500

    Additional fixes

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -145,11 +145,11 @@ resource "aws_lb_target_group" "cabal_smtp_submission_tg" {
   protocol             = "TCP"
   vpc_id               = var.vpc.id
   deregistration_delay = 30
-  stickiness           = {
+  stickiness {
     type    = source_ip
     enabled = true
   }
-  health_check         = {
+  health_check {
     enabled           = true
     interval          = 120
     port              = 587
