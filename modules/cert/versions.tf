6df77666d13cb7421e4ce0f6f63ddb44f3af5bb3
Chris <chris_carr@jake.ccarr.com>
2021-03-14 16:14:23 -0400

    Another attempt to specify version of acme provider

--- modules/cert/versions.tf
+++ modules/cert/versions.tf
@@ -1,9 +1,10 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+    acme = {
+      source = "vancluever/acme"
+      version = "2.2.0"
     }
+  }
 
   required_version = ">= 0.14.0"
 }
