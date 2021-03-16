90e943a350f96ded17feb286f768448a446c7a1e
Chris <chris_carr@jake.ccarr.com>
2021-03-15 21:37:36 -0400

    Fix stray dots

--- modules/control-domain/main.tf
+++ modules/control-domain/main.tf
@@ -11,6 +11,6 @@ resource "aws_route53_zone" "cabal_control_zone" {
 
 resource "null_resource" "cabal_fixup_nameservers" {
   provisioner "local-exec" {
-    command = "aws route53domains update-domain-nameservers --domain-name ${var.name} --nameservers Name=${aws_route53_zone.cabal_control_zone.name_servers[0]} Name=${aws_route53_zone.cabal_control_zone.name_servers.[1]} Name=${aws_route53_zone.cabal_control_zone.name_servers.[2]} Name=${aws_route53_zone.cabal_control_zone.name_servers.[3]}"
+    command = "aws route53domains update-domain-nameservers --domain-name ${var.name} --nameservers Name=${aws_route53_zone.cabal_control_zone.name_servers[0]} Name=${aws_route53_zone.cabal_control_zone.name_servers[1]} Name=${aws_route53_zone.cabal_control_zone.name_servers[2]} Name=${aws_route53_zone.cabal_control_zone.name_servers[3]}"
   }
 }
