c2be7828ad70e3631fbbf1c659bbf3b593bc8260
Chris <chris_carr@jake.ccarr.com>
2021-04-27 17:40:00 -0400

    Remove old cert

--- modules/cert-tmp/main.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  prod_url  = "https://acme-v02.api.letsencrypt.org/directory"
-  stage_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
-}
-
-provider "acme" {
-  server_url = var.prod ? local.prod_url : local.stage_url
-}
-
