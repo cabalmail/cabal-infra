0262013aac694ca6753217b3d431fc88e6a79e99
Chris <chris_carr@jake.ccarr.com>
2021-04-29 14:57:32 -0400

    DNS cert validation

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -11,6 +11,28 @@ resource "aws_acm_certificate" "cabal_elb_cert" {
   }
 }
 
+resource "aws_route53_record" "cabal_elb_cert_dns" {
+  for_each = {
+    for dvo in aws_acm_certificate.cabal_elb_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.example.zone_id
+}
+
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.cabal_elb_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cabal_elb_cert_dns : record.fqdn]
+}
+
 resource "aws_lb" "cabal_nlb" {
   name                             = "cabal-nlb"
   internal                         = false
