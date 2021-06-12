d29239f5c38ab4ac338123c32f90fe841c45a0e7
Chris <chris_carr@jake.ccarr.com>
2021-06-12 17:05:57 -0400

    Add assume role policy

--- modules/opsworks/main.tf
+++ modules/opsworks/main.tf
@@ -17,6 +17,20 @@ resource "aws_s3_bucket" "cabal_bucket" {
 }
 
 resource "aws_iam_role" "cabal_stack_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+  
   inline_policy {
     name   = "cabal-stack-policy"
     policy = <<EOF
