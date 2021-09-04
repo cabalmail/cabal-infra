2d31a8f75530a087052d8aa63c0250fa818dce9a
Chris <chris_carr@jake.ccarr.com>
2021-09-04 16:02:43 -0400

    Reference role from resource rather than data

--- modules/imap/main.tf
+++ modules/imap/main.tf
@@ -48,7 +48,7 @@ resource "aws_launch_configuration" "cabal_imap_cfg" {
   name_prefix          = "imap-"
   image_id             = data.aws_ami.amazon_linux_2.id
   instance_type        = "t2.micro"
-  iam_instance_profile = data.aws_iam_instance_profile.ssm_profile
+  iam_instance_profile = aws_iam_role.cabal_imap_role
   user_data            = <<EOD
 #!/bin/bash
 sudo yum install -y git
