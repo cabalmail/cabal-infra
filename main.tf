111a41efd494e76804efa134b75b53599f81a22c
Chris <chris_carr@jake.ccarr.com>
2021-09-04 17:59:26 -0400

    Remove repo parameter where no longer defined.

--- main.tf
+++ main.tf
@@ -10,21 +10,18 @@ module "cabal_vpc" {
   source     = "./modules/vpc"
   cidr_block = var.cidr_block
   az_list    = var.availability_zones
-  repo       = var.repo
 }
 
 module "cabal_load_balancer" {
   source         = "./modules/elb"
   public_subnets = module.cabal_vpc.public_subnets
   vpc            = module.cabal_vpc.vpc
-  repo           = var.repo
   control_domain = var.control_domain
   zone_id        = var.zone_id
 }
 
 module "cabal_efs" {
   source         = "./modules/efs"
-  repo           = var.repo
 }
 
 module "cabal_imap" {
