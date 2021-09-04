1a94344403f9b89a2c2e94387294c5ff3a1b0618
Chris <chris_carr@jake.ccarr.com>
2021-09-04 15:09:55 -0400

    Provide owners attribute to data source

--- modules/imap/main.tf
+++ modules/imap/main.tf
@@ -1,17 +1,10 @@
 data "aws_ami" "amazon-linux-2" {
- most_recent = true
-
-
- filter {
-   name   = "owner-alias"
-   values = ["amazon"]
- }
-
-
- filter {
-   name   = "name"
-   values = ["amzn2-ami-hvm*"]
- }
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
 }
 
 # TODO
