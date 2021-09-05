6a182434da403afc18b051d411ba29182234e274
Chris <chris_carr@jake.ccarr.com>
2021-09-05 10:17:07 -0400

    Force update of s3 objects on local change

--- main.tf
+++ main.tf
@@ -19,6 +19,7 @@ resource "aws_s3_bucket_object" "cabal_cookbook_files" {
   bucket = aws_s3_bucket.cabal_cookbook_bucket.bucket
   key    = each.value
   source = "${path.module}/${each.value}"
+  etag   = filemd5("${path.module}/${each.value}")
 }
 
 module "cabal_vpc" {
