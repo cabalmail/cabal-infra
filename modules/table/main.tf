b260a32c280f5f6762ca6b1912daf81cc3eefb49
Chris <chris_carr@jake.ccarr.com>
2021-09-09 14:54:22 -0400

    Add table attributes

--- modules/table/main.tf
+++ modules/table/main.tf
@@ -4,6 +4,51 @@ resource "aws_dynamodb_table" "cabal_addresses_table" {
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "address"
+  
+  attribute {
+    name = "address"
+    type = "S"
+  }
+
+  attribute {
+    name = "address"
+    type = "S"
+  }
+
+  attribute {
+    name = "subdomain"
+    type = "S"
+  }
+
+  attribute {
+    name = "comment"
+    type = "S"
+  }
+
+  attribute {
+    name = "tld"
+    type = "S"
+  }
+
+  attribute {
+    name = "user"
+    type = "S"
+  }
+
+  attribute {
+    name = "username"
+    type = "S"
+  }
+
+  attribute {
+    name = "RequestTime"
+    type = "S"
+  }
+
+  attribute {
+    name = "zone-id"
+    type = "S"
+  }
 
   ttl {
     attribute_name = "TimeToExist"
