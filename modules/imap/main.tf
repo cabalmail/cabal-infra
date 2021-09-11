5014fe6034fc4ae393fd9130b594b6104475ef50
Chris <chris_carr@jake.ccarr.com>
2021-09-10 20:10:32 -0400

    Templatize userdata and fetch certs and key

--- modules/imap/main.tf
+++ modules/imap/main.tf
@@ -99,10 +99,8 @@ resource "aws_iam_instance_profile" "cabal_imap_instance_profile" {
 }
 
 # TODO
-# Update policy to allow dynamodb table read
 # Create EC2 autoscale-groups with userdata:
 # - mount efs
-# - run chef
 
 resource "aws_security_group" "cabal_imap_sg" {
   name        = "cabal_imap_sg"
@@ -141,45 +139,10 @@ resource "aws_launch_configuration" "cabal_imap_cfg" {
   lifecycle {
     create_before_destroy = true
   }
-  user_data             = <<EOD
-#!/bin/bash -xev
-cd /tmp
-# AWS Systems Manager agent
-# yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
-# systemctl enable amazon-ssm-agent
-# systemctl start amazon-ssm-agent
-
-# AWS CLI
-yum install -y awscli
-
-# EPEL repo
-amazon-linux-extras install epel -y
-
-# Chef
-/bin/mkdir -p /etc/chef
-/bin/mkdir -p /var/lib/chef/{cookbooks,attributes}
-/bin/mkdir -p /var/log/chef
-cd /etc/chef/
-curl -L https://omnitruck.chef.io/install.sh | sudo bash
-cat > '/etc/chef/solo.rb' << EOF
-chef_license            'accept'
-log_location            STDOUT
-node_name               'imap'
-cookbook_path [ '/var/lib/chef/cookbooks' ]
-EOF
-
-cat > '/var/lib/chef/attributes/imap.json' << EOF
-{
-  "sendmail": {
-    "cert": "${var.control_domain}"
-  }
-}
-EOF
-
-aws s3 cp s3://${var.artifact_bucket}/cookbooks /var/lib/chef/cookbooks --recursive
-
-chef-solo -c /etc/chef/solo.rb -z -o "recipe[imap]" --j /var/lib/chef/attributes/imap.json
-EOD
+  user_data             = templatefile("${path.module}/userdata", {
+    control_domain  = var.control_domain,
+    artifact_bucket = var.artifact_bucket
+  })
 }
 
 resource "aws_autoscaling_group" "cabal_imap_asg" {
