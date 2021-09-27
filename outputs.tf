74e5b152419fc7926af1654fa89c47c15500037c
Chris <chris_carr@jake.ccarr.com>
2021-09-27 18:14:43 -0400

    Remove sensitive outputs

--- outputs.tf
+++ outputs.tf
@@ -6,11 +6,3 @@ output "relay_ips" {
   value       = module.cabal_vpc.relay_ips
   description = "IP addresses that will be used for outbound mail. See README.md section on PTR records for important instructions."
 }
-
-output "user_pool_id" {
-  value = module.cabal_pool.user_pool_id
-}
-
-output "user_pool_client_id" {
-  value = module.cabal_pool.user_pool_client_id
-}
