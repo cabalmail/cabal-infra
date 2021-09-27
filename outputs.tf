f4149520e422d1f25bcc251e50b1ba46d04f7b0a
Chris <chris_carr@jake.ccarr.com>
2021-09-27 18:14:21 -0400

    Add instructions to output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "IMPORTANT" {
+  value = "You must get permission from AWS to relay mail through the below IP addresses. See the section on PTR records in README.md."
+}
+
 output "relay_ips" {
   value       = module.cabal_vpc.relay_ips
   description = "IP addresses that will be used for outbound mail. See README.md section on PTR records for important instructions."
