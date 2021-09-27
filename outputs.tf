eb2bdb396ba65310c7386f96949f4acb175e62bf
Chris <chris_carr@jake.ccarr.com>
2021-09-27 18:10:12 -0400

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "relay_ips" {
-  value       = cabal_vpc.relay_ips
+  value       = module.cabal_vpc.relay_ips
   description = "IP addresses that will be used for outbound mail. See README.md section on PTR records for important instructions."
 }
 
 output "user_pool_id" {
-  value = cabal_pool.user_pool_id
+  value = module.cabal_pool.user_pool_id
 }
 
 output "user_pool_client_id" {
-  value = cabal_pool.user_pool_client_id
+  value = module.cabal_pool.user_pool_client_id
 }
