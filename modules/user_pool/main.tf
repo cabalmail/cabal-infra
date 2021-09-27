51fbc59d2cccc66d73982cb56857fda619d7adec
Chris <chris_carr@jake.ccarr.com>
2021-09-27 17:54:17 -0400

    User pool client

--- modules/user_pool/main.tf
+++ modules/user_pool/main.tf
@@ -1,3 +1,8 @@
 resource "aws_cognito_user_pool" "cabal_pool" {
   name = "cabal"
 }
+
+resource "aws_cognito_user_pool_client" "cabal_pool_client" {
+  name         = "cabal_admin_client"
+  user_pool_id = aws_cognito_user_pool.cabal_pool.id
+}
---
+++ modules/user_pool/outputs.tf
@@ -0,0 +1,7 @@
+output "user_pool_id" {
+  value = aws_cognito_user_pool.cabal_pool.id
+}
+
+output "user_pool_client_id" {
+  value = aws_cognito_user_pool_client.cabal_pool_client.id
+}
