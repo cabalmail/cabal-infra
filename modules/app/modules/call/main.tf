88b4a3716b080cef186c580c2b58c7e610e1c87e
Chris <chris_carr@jake.ccarr.com>
2021-10-07 20:31:26 -0400

    API WIP

--- modules/app/modules/call/main.tf
+++ modules/app/modules/call/main.tf
@@ -53,11 +53,31 @@ resource "aws_lambda_permission" "cabal_apigw_lambda_permission" {
   ])
 }
 
+resource "aws_iam_role" "cabal_lambda_role" {
+  name = "myrole"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
 resource "aws_lambda_function" "cabal_lambda" {
   filename = "${var.name}_lambda.zip"
   source_code_hash = data.archive_file.cabal_lambda_zip.output_base64sha256
   function_name = var.name
-  role = aws_iam_role.cabal_apigw_lambda_permission.arn
+  role = aws_iam_role.ccabal_lambda_role.arn
   handler = "index.handler"
   runtime = var.runtime
 }
