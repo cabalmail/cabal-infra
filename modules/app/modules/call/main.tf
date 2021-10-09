8c2151c6aef923ca1f78efb16e9ef9fb7c68b4a8
Chris <chris_carr@jake.ccarr.com>
2021-10-09 13:17:18 -0400

    Add response model

--- modules/app/modules/call/main.tf
+++ modules/app/modules/call/main.tf
@@ -40,6 +40,9 @@ resource "aws_api_gateway_method_response" "response_proxy" {
   resource_id = aws_api_gateway_resource.cabal_resource.id
   http_method = aws_api_gateway_method.cabal_method.http_method
   status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
 }
 
 resource "aws_lambda_permission" "cabal_apigw_lambda_permission" {
