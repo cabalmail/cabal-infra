ef30f6d73907d60c8d642fd16df69a15a4fd2253
Chris <chris_carr@jake.ccarr.com>
2021-10-11 19:15:41 -0400

    Add name servers to domain output

--- modules/domains/outputs.tf
+++ modules/domains/outputs.tf
@@ -1,8 +1,9 @@
 output "domains" {
   value = [
     for k, v in aws_route53_zone.cabal_mail_zone : {
-      "domain"  = k,
-      "zone_id" = v.id
+      "domain"       = k,
+      "zone_id"      = v.id
+      "name_servers" = v.name_servers
     }
   ]
 }
