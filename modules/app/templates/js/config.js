558e13cedc577cc3215ee6a6dcfcab3b5ef549cf
Chris <chris_carr@jake.ccarr.com>
2021-10-13 18:34:32 -0400

    Use jsonencode to deal with trailing comma

--- modules/app/templates/js/config.js
+++ modules/app/templates/js/config.js
@@ -7,8 +7,6 @@ window._config = {
     api: {
         invokeUrl: '${invoke_url}'
     },
-    domains: {
-      ${jsonencode({for domain in domains : "\"${domain.domain}\":\"${domain.zone_id}\""})}
-    }
-};
+    ${jsonencode(domains: {for domain in domains : "\"${domain.domain}\":\"${domain.zone_id}\""})}
+  };
 
