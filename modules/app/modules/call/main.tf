a378ddd9d921f9e3e0b9d5a72472451f0925b2b4
Chris <chris_carr@jake.ccarr.com>
2021-10-15 14:24:22 -0400

    Add CORS to other methods

--- modules/app/modules/call/main.tf
+++ modules/app/modules/call/main.tf
@@ -4,6 +4,25 @@ data "archive_file" "cabal_lambda_zip" {
   output_path = "${var.name}_lambda.zip"
 }
 
+locals {
+  allowed_headers = join(",", [
+    "Content-Type",
+    "X-Amz-Date",
+    "Authorization",
+    "X-Api-Key",
+    "X-Amz-Security-Token"
+  ])
+  allowed_methods = join(",", [
+    "DELETE",
+    "GET",
+    "HEAD",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT"
+  ])
+}
+
 resource "aws_api_gateway_resource" "cabal_resource" {
   rest_api_id = var.gateway_id
   parent_id   = var.root_resource_id
@@ -38,6 +57,11 @@ resource "aws_api_gateway_method_response" "cabal_response_proxy" {
   response_models = {
     "application/json" = "Empty"
   }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
   depends_on = [
     aws_api_gateway_method.cabal_method
   ]
@@ -49,6 +73,11 @@ resource "aws_api_gateway_integration_response" "cabal_integration_response" {
   resource_id = aws_api_gateway_resource.cabal_resource.id
   http_method = aws_api_gateway_method.cabal_method.http_method
   status_code = aws_api_gateway_method_response.cabal_response_proxy.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'${local.allowed_headers}'",
+    "method.response.header.Access-Control-Allow-Methods" = "'${local.allowed_methods}'",
+    "method.response.header.Access-Control-Allow-Origin"  = "'https://admin.${var.control_domain}'"
+  }
   depends_on = [
     aws_api_gateway_integration.cabal_integration
   ]
