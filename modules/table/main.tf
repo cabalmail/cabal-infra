dfdddb5893b570b5afceedacbe876682269961b0
Chris <chris_carr@jake.ccarr.com>
2021-10-16 14:31:42 -0400

    OK, we don't need the index

--- modules/table/main.tf
+++ modules/table/main.tf
@@ -4,22 +4,10 @@ resource "aws_dynamodb_table" "cabal_addresses_table" {
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "address"
-  
-  # global_secondary_index {
-  #   name               = "usernameIndex"
-  #   hash_key           = "username"
-  #   write_capacity     = 5
-  #   read_capacity      = 5
-  #   projection_type    = "ALL"
-  # }
 
   attribute {
     name = "address"
     type = "S"
   }
 
-  # attribute {
-  #   name = "username"
-  #   type = "S"
-  # }
 }
