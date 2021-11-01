263605a6b55813518d22d654d43fee7dabf9c3b7
Chris <chris_carr@jake.ccarr.com>
2021-11-01 17:08:52 -0400

    Try placing headers in the method instead of the integration

--- terraform/modules/app/modules/call/main.tf
+++ terraform/modules/app/modules/call/main.tf
@@ -36,7 +36,9 @@ resource "aws_api_gateway_method" "cabal_method" {
   authorization      = "COGNITO_USER_POOLS"
   authorizer_id      = var.authorizer
   request_parameters = {
-    "method.request.path.proxy" = true
+    "method.request.path.proxy"                   = true
+    "integration.request.header.X-Control-Domain" = "'${var.control_domain}'"
+    "integration.request.header.X-Egress-IPs"     = "'${join(" ", [for ip in var.relay_ips : "ip4:${ip}/32"])}'"
   }
 }
 
@@ -45,12 +47,8 @@ resource "aws_api_gateway_integration" "cabal_integration" {
   resource_id             = aws_api_gateway_resource.cabal_resource.id
   http_method             = aws_api_gateway_method.cabal_method.http_method
   integration_http_method = "POST"
-  type                    = "AWS"
+  type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.cabal_lambda.invoke_arn
-  request_parameters      = {
-    "integration.request.header.X-Control-Domain" = "'${var.control_domain}'"
-    "integration.request.header.X-Egress-IPs"     = "'${join(" ", [for ip in var.relay_ips : "ip4:${ip}/32"])}'"
-  }
 }
 
 resource "aws_api_gateway_method_response" "cabal_response_proxy" {
