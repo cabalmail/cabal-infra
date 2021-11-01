e7a1b45c2b569118be9028e350c14929a977c129
Chris <chris_carr@jake.ccarr.com>
2021-10-31 21:24:11 -0400

    Use non-proxy integration type

--- terraform/modules/app/modules/call/main.tf
+++ terraform/modules/app/modules/call/main.tf
@@ -45,7 +45,7 @@ resource "aws_api_gateway_integration" "cabal_integration" {
   resource_id             = aws_api_gateway_resource.cabal_resource.id
   http_method             = aws_api_gateway_method.cabal_method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = aws_lambda_function.cabal_lambda.invoke_arn
   request_parameters      = {
     "integration.request.header.X-Control-Domain" = "'${var.control_domain}'"
