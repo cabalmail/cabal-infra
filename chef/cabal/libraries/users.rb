e8b584d9430bb0cfd306b25d91770b15cb0a0869
Chris <chris_carr@jake.ccarr.com>
2021-11-09 18:56:13 -0500

    Chef library to get users from cognito

--- chef/cabal/libraries/users.rb
+++ chef/cabal/libraries/users.rb
@@ -1,6 +1,6 @@
 class CognitoUsers
   def self.list(pool_id, options)
-    require 'aws-sdk-dynamodb'
+    require 'aws-sdk-cognitoidentity'
     region = options[:region]
     cognito = Aws::CognitoIdentity::Client.new(region: region)
     resp = cognito.scan(identity_pool_id: pool_id, max_results: 1000)
