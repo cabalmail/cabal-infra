09996006e0766810263f5258853dd9163b642209
Chris <chris_carr@jake.ccarr.com>
2021-11-10 16:44:13 -0500

    Use correct target groups

--- terraform/modules/elb/outputs.tf
+++ terraform/modules/elb/outputs.tf
@@ -1,3 +1,11 @@
 output "imap_tg" {
-  value = aws_lb_target_group.cabal_imap_tg
+  value = aws_lb_target_group.cabal_imap_tg.arn
+}
+
+output "submission_tg" {
+  value = aws_lb_target_group.cabal_smtp_submission_tg.arn
+}
+
+output "relay_tg" {
+  value = aws_lb_target_group.cabal_smtp_relay_tg.arn
 }
