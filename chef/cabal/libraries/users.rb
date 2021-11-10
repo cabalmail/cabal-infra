6dfe6ffb95a626a656d5fb066c507796d82f9c5e
Chris <chris_carr@jake.ccarr.com>
2021-11-09 19:15:56 -0500

    Correct method name

--- chef/cabal/libraries/users.rb
+++ chef/cabal/libraries/users.rb
@@ -3,7 +3,7 @@ class CognitoUsers
     require 'aws-sdk-cognitoidentity'
     region = options[:region]
     cognito = Aws::CognitoIdentity::Client.new(region: region)
-    resp = cognito.scan(identity_pool_id: pool_id, max_results: 1000)
+    resp = cognito.list_identities(identity_pool_id: pool_id, max_results: 1000)
     resp.identities
   end
 end
