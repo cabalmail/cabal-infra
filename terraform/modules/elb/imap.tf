ca2518171b4b904570d41b249688238ffce55a89
Chris <chris_carr@jake.ccarr.com>
2021-11-10 14:17:23 -0500

    Big revert

--- terraform/modules/elb/imap.tf
+++ terraform/modules/elb/imap.tf
@@ -1,13 +1,17 @@
 resource "aws_lb_target_group" "cabal_imap_tg" {
   name                 = "cabal-imap-tg"
-  port                 = "993"
+  port                 = "143"
   protocol             = "TCP"
   vpc_id               = var.vpc.id
   deregistration_delay = 30
+  stickiness {
+    type    = "source_ip"
+    enabled = true
+  }
   health_check {
     enabled             = true
     interval            = 30
-    port                = 993
+    port                = 143
     protocol            = "TCP"
     healthy_threshold   = 2
     unhealthy_threshold = 2
@@ -23,8 +27,9 @@ resource "aws_lb_target_group" "cabal_imap_tg" {
 
 resource "aws_lb_listener" "cabal_imaps_listener" {
   load_balancer_arn = aws_lb.cabal_nlb.arn
-  protocol          = "TCP"
+  protocol          = "TLS"
   port              = "993"
+  certificate_arn   = var.cert_arn
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.cabal_imap_tg.arn
