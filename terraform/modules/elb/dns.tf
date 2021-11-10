788590e72b742a0c1a02b5c1f291b89645b1dba8
Chris <chris_carr@jake.ccarr.com>
2021-11-09 20:15:26 -0500

    CNAMES for load balancer

--- terraform/modules/elb/dns.tf
+++ terraform/modules/elb/dns.tf
@@ -1,26 +1,23 @@
 resource "aws_route53_record" "cabal_imap_cname" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = var.zone_id
   name    = "imap"
   type    = "CNAME"
   ttl     = "900"
-
-  records        = [aws_lb.cabal_nlb.dns_name]
+  records = [aws_lb.cabal_nlb.dns_name]
 }
 
 resource "aws_route53_record" "cabal_smtpout_cname" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = var.zone_id
   name    = "smtp-out"
   type    = "CNAME"
   ttl     = "900"
-
-  records        = [aws_lb.cabal_nlb.dns_name]
+  records = [aws_lb.cabal_nlb.dns_name]
 }
 
 resource "aws_route53_record" "cabal_smtpin_cname" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = var.zone_id
   name    = "smtp-in"
   type    = "CNAME"
   ttl     = "900"
-
-  records        = [aws_lb.cabal_nlb.dns_name]
+  records = [aws_lb.cabal_nlb.dns_name]
 }
