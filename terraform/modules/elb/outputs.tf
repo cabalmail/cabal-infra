e871115af6ce2172e2c624c759e87c031e7e5afc
Chris <chris_carr@jake.ccarr.com>
2021-11-10 17:20:19 -0500

    Export starttls target group ARN

--- terraform/modules/elb/outputs.tf
+++ terraform/modules/elb/outputs.tf
@@ -6,6 +6,10 @@ output "submission_tg" {
   value = aws_lb_target_group.cabal_smtp_submission_tg.arn
 }
 
+output "starttls_tg" {
+  value = aws_lb_target_group.cabal_smtp_starttls_tg.arn
+}
+
 output "relay_tg" {
   value = aws_lb_target_group.cabal_smtp_relay_tg.arn
 }
