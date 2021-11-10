23d92dbce7162851409ea0d9073355973486c325
Chris <chris_carr@jake.ccarr.com>
2021-11-10 13:53:52 -0500

    Try offloading SSL at the node for IMAP

--- terraform/modules/elb/imap.tf
+++ terraform/modules/elb/imap.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "cabal_imap_tg" {
   name                 = "cabal-imap-tg"
-  port                 = "143"
+  port                 = "993"
   protocol             = "TCP"
   vpc_id               = var.vpc.id
   deregistration_delay = 30
@@ -11,7 +11,7 @@ resource "aws_lb_target_group" "cabal_imap_tg" {
   health_check {
     enabled             = true
     interval            = 30
-    port                = 143
+    port                = 993
     protocol            = "TCP"
     healthy_threshold   = 2
     unhealthy_threshold = 2
@@ -27,9 +27,8 @@ resource "aws_lb_target_group" "cabal_imap_tg" {
 
 resource "aws_lb_listener" "cabal_imaps_listener" {
   load_balancer_arn = aws_lb.cabal_nlb.arn
-  protocol          = "TLS"
+  protocol          = "TCP"
   port              = "993"
-  certificate_arn   = var.cert_arn
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.cabal_imap_tg.arn
