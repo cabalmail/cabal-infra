dd7eb0d0f7baaf25ff741acf8341f7db05acf604
Chris <chris_carr@jake.ccarr.com>
2021-11-12 16:46:44 -0500

    Format parameter correctly

--- chef/cabal/libraries/route53.rb
+++ chef/cabal/libraries/route53.rb
@@ -4,24 +4,24 @@ class Route53Record
     region = options[:region]
     route53 = Aws::Route53::Client.new(region: region)
     resp = route53.change_resource_record_sets({
-      changes: [
-        {
-          action: "UPSERT", 
-          resource_record_set: {
-            name: name, 
-            resource_records: [
-              {
-                value: value, 
-              }, 
-            ], 
-            ttl: ttl, 
-            type: type, 
+      change_batch: {
+        changes: [
+          {
+            action: "UPSERT",
+            resource_record_set: {
+              name: name,
+              resource_records: [
+                {
+                  value: value,
+                },
+              ],
+              ttl: ttl,
+              type: type,
+            },
           },
-        },
-      ],
-      hosted_zone_id: zone_id,
+        ],
+      },
+      hosted_zone_id: zone_id,   
     })
-    # resp = dynamodb.scan(table_name: table)
-    # resp.items
   end
 end
