e9856cd0181664f9c5bbc3b3b444fdfae3fa562e
Chris <chris_carr@jake.ccarr.com>
2021-11-12 20:56:41 -0500

    Ship cookbook as zip archive to reduce calls to S3

--- terraform/modules/cookbook/main.tf
+++ terraform/modules/cookbook/main.tf
@@ -3,11 +3,23 @@ resource "aws_s3_bucket" "cabal_cookbook_bucket" {
   bucket_prefix = "cabal-artifacts-"
 }
 
-resource "aws_s3_bucket_object" "cabal_cookbook_files" {
-  for_each = fileset(path.module, "../../../chef/cabal/**/*")
-
-  bucket   = aws_s3_bucket.cabal_cookbook_bucket.bucket
-  key      = each.value
-  source   = "${path.module}/${each.value}"
-  etag     = filemd5("${path.module}/${each.value}")
+data "archive_file" "cabal_cookbook_archive" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../../chef/cabal"
+  output_path = "${path.module}/cabal_cookbook.zip"
 }
+
+resource "aws_s3_bucket_object" "cabal_cookbook_object" {
+  bucket   = aws_s3_bucket.cabal_cookbook_bucket.bucket
+  key      = "/cabal.zip"
+  source   = "${path.module}/cabal_cookbook.zip"
+}
+
+# resource "aws_s3_bucket_object" "cabal_cookbook_files" {
+#   for_each = fileset(path.module, "../../../chef/cabal/**/*")
+
+#   bucket   = aws_s3_bucket.cabal_cookbook_bucket.bucket
+#   key      = each.value
+#   source   = "${path.module}/${each.value}"
+#   etag     = filemd5("${path.module}/${each.value}")
+# }
