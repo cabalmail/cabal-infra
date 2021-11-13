663b07e720016142d7b7c3c6e8b19de3e020d6f7
Chris <chris_carr@jake.ccarr.com>
2021-11-12 20:52:58 -0500

    Set consistent UIDs

--- chef/cabal/recipes/_common_users.rb
+++ chef/cabal/recipes/_common_users.rb
@@ -1,29 +1,33 @@
 users = []
+uid   = 2000
 CognitoUsers.list(node['cognito']['pool_id'], { region: node['ec2']['region'] }).each do |user|
-  users.push(user.username)
+  users.push(user)
 end
-
+users.sort_by { |u| u.user_create_date }
 users.each do |u|
-  user u
-  directory "/home/#{u}" do
-    owner u
-    group u
+  user u.username do
+    uid uid
+  end
+  directory "/home/#{u.username}" do
+    owner u.username
+    group u.username
     mode 0700
   end
-  directory "/home/#{u}/Maildir" do
-    owner u
-    group u
+  directory "/home/#{u.username}/Maildir" do
+    owner u.username
+    group u.username
     mode 0700
   end
-  directory "/home/#{u}/.procmail" do
-    owner u
-    group u
+  directory "/home/#{u.username}/.procmail" do
+    owner u.username
+    group u.username
     mode 0755
   end
-  cookbook_file "/home/#{u}/.procmailrc" do
+  cookbook_file "/home/#{u.username}/.procmailrc" do
     source 'procmailrc'
-    owner u
-    group u
+    owner u.username
+    group u.username
     mode 0744
   end
+  uid += 1
 end
