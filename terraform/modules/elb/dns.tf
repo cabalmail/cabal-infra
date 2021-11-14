fa2e3b21255646e35e90b78abeed6a0d73c3c3c0
Chris <chris_carr@jake.ccarr.com>
2021-11-14 10:59:57 -0500

    Convert CNAMEs to A with Alias

--- terraform/modules/elb/dns.tf
+++ terraform/modules/elb/dns.tf
@@ -1,23 +1,35 @@
 resource "aws_route53_record" "cabal_imap_cname" {
   zone_id = var.zone_id
   name    = "imap"
-  type    = "CNAME"
-  ttl     = "900"
-  records = [aws_lb.cabal_nlb.dns_name]
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.cabal_nlb.dns_name
+    zone_id                = aws_lb.cabal_nlb.zone_id
+    evaluate_target_health = true
+  }
 }
 
 resource "aws_route53_record" "cabal_smtpout_cname" {
   zone_id = var.zone_id
   name    = "smtp-out"
-  type    = "CNAME"
-  ttl     = "900"
-  records = [aws_lb.cabal_nlb.dns_name]
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.cabal_nlb.dns_name
+    zone_id                = aws_lb.cabal_nlb.zone_id
+    evaluate_target_health = true
+  }
 }
 
 resource "aws_route53_record" "cabal_smtpin_cname" {
   zone_id = var.zone_id
   name    = "smtp-in"
-  type    = "CNAME"
-  ttl     = "900"
-  records = [aws_lb.cabal_nlb.dns_name]
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.cabal_nlb.dns_name
+    zone_id                = aws_lb.cabal_nlb.zone_id
+    evaluate_target_health = true
+  }
 }
