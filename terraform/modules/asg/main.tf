570ee014b7bc6649edb63165669a073feaca440b
Chris <chris_carr@jake.ccarr.com>
2021-11-14 08:26:58 -0500

    Fix text alignment

--- terraform/modules/asg/main.tf
+++ terraform/modules/asg/main.tf
@@ -21,12 +21,12 @@ resource "aws_launch_configuration" "cabal_cfg" {
 }
 
 resource "aws_autoscaling_group" "cabal_asg" {
-  vpc_zone_identifier   = var.private_subnets[*].id
-  desired_capacity      = var.scale.des
-  max_size              = var.scale.max
-  min_size              = var.scale.min
-  launch_configuration  = aws_launch_configuration.cabal_cfg.id
-  target_group_arns     = var.target_groups
+  vpc_zone_identifier  = var.private_subnets[*].id
+  desired_capacity     = var.scale.des
+  max_size             = var.scale.max
+  min_size             = var.scale.min
+  launch_configuration = aws_launch_configuration.cabal_cfg.id
+  target_group_arns    = var.target_groups
   instance_refresh {
     strategy = "Rolling"
     preferences {
