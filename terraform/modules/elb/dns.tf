5bb586c6a90a1f0c5ae656643424326448572ad7
Chris <chris_carr@jake.ccarr.com>
2021-11-16 17:45:30 -0500

    Consolidate DNS

--- terraform/modules/elb/dns.tf
+++ terraform/modules/elb/dns.tf
@@ -1,31 +1,8 @@
-resource "aws_route53_record" "cabal_imap_cname" {
-  zone_id = var.zone_id
-  name    = "imap"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.cabal_nlb.dns_name
-    zone_id                = aws_lb.cabal_nlb.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "cabal_smtpout_cname" {
-  zone_id = var.zone_id
-  name    = "smtp-out"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.cabal_nlb.dns_name
-    zone_id                = aws_lb.cabal_nlb.zone_id
-    evaluate_target_health = false
-  }
-}
-
-resource "aws_route53_record" "cabal_smtpin_cname" {
-  zone_id = var.zone_id
-  name    = "smtp-in"
-  type    = "A"
+resource "aws_route53_record" "cabal_cname" {
+  for_each = toset( ["imap", "smtp-out", "smtp-in"] )
+  zone_id  = var.zone_id
+  name     = each.key
+  type     = "A"
 
   alias {
     name                   = aws_lb.cabal_nlb.dns_name
