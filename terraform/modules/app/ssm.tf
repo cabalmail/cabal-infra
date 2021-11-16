22b97a4069ee02aebe36d81a7e0a1b322020f94d
Chris <chris_carr@jake.ccarr.com>
2021-11-15 19:36:31 -0500

    Use schema version 2.2

--- terraform/modules/app/ssm.tf
+++ terraform/modules/app/ssm.tf
@@ -4,26 +4,47 @@ resource "aws_ssm_document" "cabal_document" {
   document_type = "Command"
   content = <<DOC
   {
+
     "schemaVersion": "2.2",
+
     "description": "Run chef-solo",
-      "parameters": {
-        "commands": {
-          "type": "String",
-          "description": "Run chef-solo on ${each.key} machines",
-          "default": "chef-solo -c /etc/chef/solo.rb -z -o 'recipe[cabal::${each.key}]' -j /var/lib/chef/attributes/node.json"
-        }
+
+    "parameters": {
+
+      "commands": {
+
+        "type": "String",
+
+        "description": "Run chef-solo on ${each.key} machines",
+
+        "default": "chef-solo -c /etc/chef/solo.rb -z -o 'recipe[cabal::${each.key}]' -j /var/lib/chef/attributes/node.json"
+
+      }
+
       },
+
       "mainSteps": [
-        "action": "aws:runShellScript",
-        "name": "runShellScript",
-        "inputs": {
-          "timeoutSeconds": "300",
-          "runCommand": [
-            "{{ commands }}"
-          ]
-        }
+
+      "action": "aws:runShellScript",
+
+      "name": "runShellScript",
+
+      "inputs": {
+
+        "timeoutSeconds": "300",
+
+        "runCommand": [
+
+        "{{ commands }}"
+
+        ]
+
+      }
+
       ]
+
     }
+
   }
 DOC
 }
