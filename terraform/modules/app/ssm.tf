ccc8c306b30931a9bcb912005e6fb360046d6a3c
Chris <chris_carr@jake.ccarr.com>
2021-11-15 19:22:44 -0500

    Use schema version 2.2

--- terraform/modules/app/ssm.tf
+++ terraform/modules/app/ssm.tf
@@ -4,11 +4,11 @@ resource "aws_ssm_document" "cabal_document" {
   document_type = "Command"
   content = <<DOC
   {
-    "schemaVersion": "2.0",
+    "schemaVersion": "2.2",
     "description": "Run chef-solo",
     "parameters": {
     },
-    "runtimeConfig": {
+    "mainSteps": {
       "aws:runShellScript": {
         "properties": [
           {
