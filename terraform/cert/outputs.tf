1ca4909a67b25df00d5d43c73ec6eed1049eb49a
Chris <chris_carr@jake.ccarr.com>
2021-11-25 21:37:39 -0500

    Refactor with less redundant resource names

--- terraform/cert/outputs.tf
+++ terraform/cert/outputs.tf
@@ -1,16 +1,16 @@
 output "public_key" {
-  value = tls_private_key.cabal_cert_private_key.public_key_pem
+  value = tls_private_key.pk.public_key_pem
 }
 
 output "private_key" {
-  value = tls_private_key.cabal_cert_private_key.private_key_pem
+  value = tls_private_key.pk.private_key_pem
   sensitive = true
 }
 
 output "cert" {
-  value = acme_certificate.cabal_certificate.certificate_pem
+  value = acme_certificate.cert.certificate_pem
 }
 
 output "intermediate" {
-  value = acme_certificate.cabal_certificate.issuer_pem
+  value = acme_certificate.cert.issuer_pem
 }
