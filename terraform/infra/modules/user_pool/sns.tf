53475c9e687c84a20e0dcc27debcfc666e4a0cbe
Chris <chris_carr@jake.ccarr.com>
2021-11-25 22:46:03 -0500

    Refactor with less redundant resource names

--- terraform/infra/modules/user_pool/sns.tf
+++ terraform/infra/modules/user_pool/sns.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "users" {
   name               = "cabal_sns_role"
-  assume_role_policy = "${data.aws_iam_policy_document.cabal_sns_assume_role_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.users.json
 }
 
 data "aws_iam_policy_document" "cabal_sns_publish_policy" {
@@ -17,10 +17,10 @@ data "aws_iam_policy_document" "cabal_sns_publish_policy" {
 
 resource "aws_iam_policy" "users" {
   name   = "cabal_sns_role_policy"
-  policy = "${data.aws_iam_policy_document.users.json}"
+  policy = data.aws_iam_policy_document.users.json
 }
 
 resource "aws_iam_role_policy_attachment" "users" {
-  role       = "${aws_iam_role.users.name}"
-  policy_arn = "${aws_iam_policy.users.arn}"
+  role       = aws_iam_role.users.name
+  policy_arn = aws_iam_policy.users.arn
 }
