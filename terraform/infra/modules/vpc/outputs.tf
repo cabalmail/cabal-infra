a94b5b8b67cd6ec8a392076be7686d744a1d0c45
Chris <chris_carr@jake.ccarr.com>
2021-11-26 13:15:40 -0500

    Test create/destroy cycle on null_resource

--- terraform/infra/modules/vpc/outputs.tf
+++ terraform/infra/modules/vpc/outputs.tf
@@ -2,18 +2,18 @@ output "vpc" {
   value = aws_vpc.network
 }
 
-# output "private_subnets" {
-#   value = aws_subnet.private[*]
-# }
+output "private_subnets" {
+  value = aws_subnet.private[*]
+}
 
-# output "public_subnets" {
-#   value = aws_subnet.public[*]
-# }
+output "public_subnets" {
+  value = aws_subnet.public[*]
+}
 
-# output "relay_ips" {
-#   value = aws_eip.nat_eip[*].public_ip
-# }
+output "relay_ips" {
+  value = aws_eip.nat_eip[*].public_ip
+}
 
-# output "private_zone" {
-#   value = aws_route53_zone.private_dns
-# }
+output "private_zone" {
+  value = aws_route53_zone.private_dns
+}
--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -1,18 +1,18 @@
-output "IMPORTANT" {
-  value = [
-    "You must get permission from AWS to relay mail through the below IP addresses. See the section on PTR records in README.md.",
-    "You must update your domain registrations with the name servers from the below domains. See the section on Name Servers in README.md"
-  ]
-}
+# output "IMPORTANT" {
+#   value = [
+#     "You must get permission from AWS to relay mail through the below IP addresses. See the section on PTR records in README.md.",
+#     "You must update your domain registrations with the name servers from the below domains. See the section on Name Servers in README.md"
+#   ]
+# }
 
-output "relay_ips" {
-  value       = {
-    addresses = module.cabal_vpc.relay_ips
-    domain    = "smtp.${var.control_domain}"
-  }
-  description = "IP addresses that will be used for outbound mail. See README.md section on PTR records for important instructions."
-}
+# output "relay_ips" {
+#   value       = {
+#     addresses = module.cabal_vpc.relay_ips
+#     domain    = "smtp.${var.control_domain}"
+#   }
+#   description = "IP addresses that will be used for outbound mail. See README.md section on PTR records for important instructions."
+# }
 
-output "domains" {
-  value = module.cabal_domains.domains
-}
+# output "domains" {
+#   value = module.cabal_domains.domains
+# }
