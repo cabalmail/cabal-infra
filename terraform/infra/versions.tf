33e18bd80408a6112af24941daf726b963ea067f
Chris <chris_carr@jake.ccarr.com>
2021-11-27 13:49:57 -0500

    Update version of aws provider

--- terraform/infra/versions.tf
+++ terraform/infra/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.57.0"
+      version = "~> 3.67.0"
     }
     acme = {
       source  = "vancluever/acme"
