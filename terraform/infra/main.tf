202f945d9b4ea6bd5a3c133031752bd128c67e67
Chris <chris_carr@jake.ccarr.com>
2021-11-27 18:54:34 -0500

    Declare autoscale groups' dependency on cert module

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -9,6 +9,7 @@ provider "aws" {
 }
 
 # Creates an AWS Certificate Manager certificate for use on load balancers and CloudFront
+# and requests a Let's Encrypt certificate for use on EC2 instances
 module "cert" {
   source         = "./modules/cert"
   control_domain = var.control_domain
@@ -50,7 +51,7 @@ module "admin" {
 
 # Creates a DynamoDB table for storing address data
 module "table" {
-  source     = "./modules/table"
+  source = "./modules/table"
 }
 
 # Creates the VPC and network infrastructure
@@ -74,82 +75,85 @@ module "load_balancer" {
 
 # Creates an elastic file system for the mailstore
 module "efs" {
-  source           = "./modules/efs"
-  vpc              = module.vpc.vpc
-  private_subnets  = module.vpc.private_subnets
+  source          = "./modules/efs"
+  vpc             = module.vpc.vpc
+  private_subnets = module.vpc.private_subnets
 }
 
 # Creates an auto-scale group for IMAP servers
 module "imap" {
-  source           = "./modules/asg"
-  type             = "imap"
-  private_subnets  = module.vpc.private_subnets
-  vpc              = module.vpc.vpc
-  control_domain   = var.control_domain
-  artifact_bucket  = module.cookbook.bucket.id
-  target_groups    = [module.load_balancer.imap_tg]
-  table_arn        = module.table.table_arn
-  s3_arn           = module.cookbook.bucket.arn
-  efs_dns          = module.efs.efs_dns
-  user_pool_arn    = module.pool.user_pool_arn
-  region           = var.aws_region
-  ports            = [143, 993]
-  private_ports    = [25]
-  cidr_block       = var.cidr_block
-  private_zone     = module.vpc.private_zone
-  client_id        = module.pool.user_pool_client_id
-  user_pool_id     = module.pool.user_pool_id
-  scale            = var.imap_scale
-  chef_license     = var.chef_license
+  source          = "./modules/asg"
+  type            = "imap"
+  private_subnets = module.vpc.private_subnets
+  vpc             = module.vpc.vpc
+  control_domain  = var.control_domain
+  artifact_bucket = module.cookbook.bucket.id
+  target_groups   = [module.load_balancer.imap_tg]
+  table_arn       = module.table.table_arn
+  s3_arn          = module.cookbook.bucket.arn
+  efs_dns         = module.efs.efs_dns
+  user_pool_arn   = module.pool.user_pool_arn
+  region          = var.aws_region
+  ports           = [143, 993]
+  private_ports   = [25]
+  cidr_block      = var.cidr_block
+  private_zone    = module.vpc.private_zone
+  client_id       = module.pool.user_pool_client_id
+  user_pool_id    = module.pool.user_pool_id
+  scale           = var.imap_scale
+  chef_license    = var.chef_license
+  depends_on      = [ module.cert ]
 }
 
 # Creates an auto-scale group for inbound SMTP servers
 module "smtp_in" {
-  source           = "./modules/asg"
-  type             = "smtp-in"
-  private_subnets  = module.vpc.private_subnets
-  vpc              = module.vpc.vpc
-  control_domain   = var.control_domain
-  artifact_bucket  = module.cookbook.bucket.id
-  target_groups    = [module.load_balancer.relay_tg]
-  table_arn        = module.table.table_arn
-  s3_arn           = module.cookbook.bucket.arn
-  efs_dns          = module.efs.efs_dns
-  region           = var.aws_region
-  ports            = [25, 465, 587]
-  private_ports    = []
-  cidr_block       = var.cidr_block
-  private_zone     = module.vpc.private_zone
-  client_id        = module.pool.user_pool_client_id
-  user_pool_id     = module.pool.user_pool_id
-  user_pool_arn    = module.pool.user_pool_arn
-  scale            = var.smtpin_scale
-  chef_license     = var.chef_license
+  source          = "./modules/asg"
+  type            = "smtp-in"
+  private_subnets = module.vpc.private_subnets
+  vpc             = module.vpc.vpc
+  control_domain  = var.control_domain
+  artifact_bucket = module.cookbook.bucket.id
+  target_groups   = [module.load_balancer.relay_tg]
+  table_arn       = module.table.table_arn
+  s3_arn          = module.cookbook.bucket.arn
+  efs_dns         = module.efs.efs_dns
+  region          = var.aws_region
+  ports           = [25, 465, 587]
+  private_ports   = []
+  cidr_block      = var.cidr_block
+  private_zone    = module.vpc.private_zone
+  client_id       = module.pool.user_pool_client_id
+  user_pool_id    = module.pool.user_pool_id
+  user_pool_arn   = module.pool.user_pool_arn
+  scale           = var.smtpin_scale
+  chef_license    = var.chef_license
+  depends_on      = [ module.cert ]
 }
 
 # Creates an auto-scale group for outbound SMTP servers
 module "smtp_out" {
-  source           = "./modules/asg"
-  type             = "smtp-out"
-  private_subnets  = module.vpc.private_subnets
-  vpc              = module.vpc.vpc
-  control_domain   = var.control_domain
-  artifact_bucket  = module.cookbook.bucket.id
-  target_groups    = [
+  source          = "./modules/asg"
+  type            = "smtp-out"
+  private_subnets = module.vpc.private_subnets
+  vpc             = module.vpc.vpc
+  control_domain  = var.control_domain
+  artifact_bucket = module.cookbook.bucket.id
+  target_groups   = [
     module.load_balancer.submission_tg,
     module.load_balancer.starttls_tg
   ]
-  table_arn        = module.table.table_arn
-  s3_arn           = module.cookbook.bucket.arn
-  efs_dns          = module.efs.efs_dns
-  region           = var.aws_region
-  ports            = [25, 465, 587]
-  private_ports    = []
-  cidr_block       = var.cidr_block
-  private_zone     = module.vpc.private_zone
-  client_id        = module.pool.user_pool_client_id
-  user_pool_id     = module.pool.user_pool_id
-  user_pool_arn    = module.pool.user_pool_arn
-  scale            = var.smtpout_scale
-  chef_license     = var.chef_license
-  }
+  table_arn       = module.table.table_arn
+  s3_arn          = module.cookbook.bucket.arn
+  efs_dns         = module.efs.efs_dns
+  region          = var.aws_region
+  ports           = [25, 465, 587]
+  private_ports   = []
+  cidr_block      = var.cidr_block
+  private_zone    = module.vpc.private_zone
+  client_id       = module.pool.user_pool_client_id
+  user_pool_id    = module.pool.user_pool_id
+  user_pool_arn   = module.pool.user_pool_arn
+  scale           = var.smtpout_scale
+  chef_license    = var.chef_license
+  depends_on      = [ module.cert ]
+}
