11134f37e1e88ed1274fc4e94eba0b2688ab75bd
Chris <chris_carr@jake.ccarr.com>
2021-11-27 11:25:30 -0500

    Consolidate cert and infra stacks

--- terraform/infra/versions.tf
+++ terraform/infra/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 3.57.0"
     }
+    aws  = {
+      source  = "hashicorp/aws"
+      version = "~> 3.33.0"
+    }
   }
 
   required_version = ">= 1.0.0"
