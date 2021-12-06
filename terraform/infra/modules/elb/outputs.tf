ad454d24740a93ea065c3101731b7338ebb505f9
Chris <chris_carr@jake.ccarr.com>
2021-12-05 20:02:14 -0500

    Specify types for some child module variables

--- terraform/infra/modules/elb/outputs.tf
+++ terraform/infra/modules/elb/outputs.tf
@@ -1,15 +1,19 @@
 output "imap_tg" {
-  value = aws_lb_target_group.imap.arn
+  value       = aws_lb_target_group.imap.arn
+  description = "ARN of IMAP target group."
 }
 
 output "submission_tg" {
-  value = aws_lb_target_group.submission.arn
+  value       = aws_lb_target_group.submission.arn
+  description = "ARN of SMTP submission target group."
 }
 
 output "starttls_tg" {
-  value = aws_lb_target_group.starttls.arn
+  value       = aws_lb_target_group.starttls.arn
+  description = "ARN of SMTP StartTLS target group."
 }
 
 output "relay_tg" {
-  value = aws_lb_target_group.relay.arn
+  value       = aws_lb_target_group.relay.arn
+  description = "ARN of SMTP relay target group."
 }
--- terraform/infra/modules/elb/variables.tf
+++ terraform/infra/modules/elb/variables.tf
@@ -3,17 +3,21 @@ variable "control_domain" {
 }
 
 variable "vpc" {
+  type        = map
   description = "VPC for the load balancer."
 }
 
 variable "public_subnets" {
+  type        = list(map)
   description = "Subnets for load balancer targets."
 }
 
 variable "zone_id" {
+  type        = string
   description = "Route 53 Zone ID for control domain"
 }
 
 variable "cert_arn" {
-  type = string
+  type        = string
+  description = "ARN of AWS Certificate Manager certificate."
 }
