f4d8bf6012d5bb79a4d0ca69f9c007f2da25df92
Chris <chris_carr@jake.ccarr.com>
2021-12-05 20:16:39 -0500

    Simplify variables in elb module

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -81,7 +81,7 @@ module "load_balancer" {
 // Creates an elastic file system for the mailstore
 module "efs" {
   source             = "./modules/efs"
-  vpc_id             = module.vpc.vpc.vpc_id
+  vpc_id             = module.vpc.vpc.id
   vpc_cidr_block     = module.vpc.vpc.cidr_block
   private_subnet_ids = module.vpc.private_subnets[*].id
 }
