55349b8d49c080bcafec803e10e097d16ef5bd91
Chris <chris_carr@jake.ccarr.com>
2021-12-05 20:01:32 -0500

    Pass list of simple IDs instead of entire objects

--- terraform/infra/modules/efs/main.tf
+++ terraform/infra/modules/efs/main.tf
@@ -28,7 +28,7 @@ resource "aws_security_group" "mailstore" {
  }
 
 resource "aws_efs_mount_target" "mailstore" {
-  count           = length(var.private_subnets)
+  count           = length(var.private_subnet_ids)
   file_system_id  = aws_efs_file_system.mailstore.id
   subnet_id       = var.private_subnet_ids[count.index]
   security_groups = [aws_security_group.mailstore.id]
