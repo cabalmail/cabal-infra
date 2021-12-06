8fdfefb9dab53456a0052ba19e5d7d67ac8d4705
Chris <chris_carr@jake.ccarr.com>
2021-12-05 20:05:07 -0500

    Pass separate ID and cidr block for VPC instead of entire VPC object.

--- terraform/infra/modules/efs/main.tf
+++ terraform/infra/modules/efs/main.tf
@@ -10,17 +10,17 @@ resource "aws_efs_file_system" "mailstore" {
 
 resource "aws_security_group" "mailstore" {
    name   = "cabal-efs-sg"
-   vpc_id = var.vpc.id
+   vpc_id = var.vpc_id
 
    ingress {
-     cidr_blocks = [var.vpc.cidr_block]
+     cidr_blocks = [var.vpc_cidr_block]
      from_port   = 2049
      to_port     = 2049
      protocol    = "tcp"
    }
 
    egress {
-     cidr_blocks = [var.vpc.cidr_block]
+     cidr_blocks = [var.vpc_cidr_block]
      from_port   = 0
      to_port     = 0
      protocol    = -1
--- terraform/infra/modules/efs/variables.tf
+++ terraform/infra/modules/efs/variables.tf
@@ -1,6 +1,11 @@
-variable "vpc" {
-  type        = map
-  description = "VPC"
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "CIDR block for the VPC"
 }
 
 variable "private_subnet_ids" {
