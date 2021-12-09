ae34847bb94f94c303edffd891232990b1d51012
Chris <chris_carr@jake.ccarr.com>
2021-12-09 18:58:36 -0500

    Proper ruby symbol syntax

--- chef/cabal/recipes/_common.rb
+++ chef/cabal/recipes/_common.rb
@@ -4,7 +4,7 @@ template '/usr/bin/cognito.bash' do
   group 'root'
   source 'cognito.bash.erb'
   variables(
-    region => node['cognito']['region'],
-    client_id => node['cognito']['client_id']
+    :region => node['cognito']['region'],
+    :client_id => node['cognito']['client_id']
   )
 end
