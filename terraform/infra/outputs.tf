d22438af25e4957a523f8a4ab149f1687f73130c
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2021-12-10 06:05:59 -0500

    Output description

--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -1,8 +1,9 @@
 output "IMPORTANT" {
-  value = [
+  value       = [
     "You must get permission from AWS to relay mail through the below IP addresses. See the section on Port 25 in docs/setup.md.",
     "You must update your domain registrations with the name servers from the below domains. See the section on Nameservers in docs/setup.md"
   ]
+	description = "Instructions for post-automation steps."
 }
 
 output "relay_ips" {
