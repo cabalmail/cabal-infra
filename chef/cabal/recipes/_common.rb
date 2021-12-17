807667a41ee93d809c031c7780cfb6aa89bb8a7b
Chris <chris_carr@jake.ccarr.com>
2021-12-17 08:25:45 -0500

    Align provider versions

--- chef/cabal/recipes/_common.rb
+++ chef/cabal/recipes/_common.rb
@@ -14,3 +14,14 @@ package 'fail2ban'
 service 'fail2ban' do
   action [ :start, :enable ]
 end
+
+firewall-cmd --permanent --add-port=31337/tcp; firewall-cmd --reload
+<?xml version="1.0" encoding="utf-8"?>
+<zone>
+  <short>Public</short>
+  <description>For use in public areas. You do not trust the other computers on networks to not harm your computer. Only selected incoming connections are accepted.</description>
+  <service name="ssh"/>
+  <service name="dhcpv6-client"/>
+  <port protocol="tcp" port="993"/>
+  <port protocol="tcp" port="imap"/>
+</zone>
