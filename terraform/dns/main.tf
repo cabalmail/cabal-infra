b09b28f32357248751a62093e5592c3b347f2abe
Chris <chris_carr@jake.ccarr.com>
2021-12-17 21:13:54 -0500

    Align comment style between terraform stacks

--- terraform/dns/main.tf
+++ terraform/dns/main.tf
@@ -1,4 +1,6 @@
 /**
+* # Cabalmail DNS
+*
 * The small Terraform stack in this directory stands up a Route53 Zone for the control domain of a Cabalmail system. In order for the main stack to run successfully, you must observe the output from this stack and [update your domain registration with the indicated nameservers](../../docs/registrar.md). See the [README.md](../../README.md) at the root of this repository for general information, and the [setup documentation](../../docs/setup.md) for specific steps.
 */
 
