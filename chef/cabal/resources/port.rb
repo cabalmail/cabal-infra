4590809b89c2e4f513300681d08345090e7e428a
Chris <chris_carr@jake.ccarr.com>
2021-12-19 16:01:24 -0500

    Add missing protocol to firewall command

--- chef/cabal/resources/port.rb
+++ chef/cabal/resources/port.rb
@@ -4,18 +4,18 @@ property :port, String, name_property: true, required: true
 
 action :open do
   execute "add port #{new_resource.port} to zone" do
-    not_if "firewall-cmd --permanent --zone=public --query-port=#{new_resource.port}"
+    not_if "firewall-cmd --permanent --zone=public --query-port=#{new_resource.port}/tcp"
     command(<<-EOC)
-    firewall-cmd --permanent --zone=public --add-port=#{new_resource.port}
+    firewall-cmd --permanent --zone=public --add-port=#{new_resource.port}/tcp
     EOC
   end
 end
 
 action :close do
   execute "add port #{new_resource.port} to zone" do
-    only_if "firewall-cmd --permanent --zone=public --query-port=#{new_resource.port}"
+    only_if "firewall-cmd --permanent --zone=public --query-port=#{new_resource.port}/tcp"
     command(<<-EOC)
-    firewall-cmd --permanent --zone=public --remove-port=#{new_resource.port}
+    firewall-cmd --permanent --zone=public --remove-port=#{new_resource.port}/tcp
     EOC
   end
 end
