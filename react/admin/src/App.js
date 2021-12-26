b174705d176b89652717346d8a01f50c4c109cb5
Chris <chris_carr@jake.ccarr.com>
2021-12-26 15:10:19 -0500

    Temporarily comment out cognito library

--- react/admin/src/App.js
+++ react/admin/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
+// import AmazonCognitoIdentity from 'amazon-cognito-identity-js';
 import Request from './Request.js';
 import List from './List.js';
 import SignUp from './SignUp.js';
