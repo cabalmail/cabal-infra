88a8acbc263101407eb1f64210e000376573a025
Chris <chris_carr@jake.ccarr.com>
2021-12-26 20:58:15 -0500

    Organize output for better readability

--- terraform/dns/outputs.tf
+++ terraform/dns/outputs.tf
@@ -1,15 +1,11 @@
-output "zone_id" {
-  value = aws_route53_zone.cabal_control_zone.zone_id
-}
-
-output "name_servers" {
+output "Update the registration for your control domain with these name servers. See README.md for more information." {
   value = aws_route53_zone.cabal_control_zone.name_servers
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.react_app.id
-}
-
-output "cognito" {
-  value       = module.pool
+output "Create environment variables with your Github repo. See README.md for more information."{
+  value = {
+    COGNITO_USER_POOL_ID = module.pool.user_pool_id
+    COGNITO_CLIENT_ID    = module.pool.user_pool_client_id
+    AWS_S3_BUCKET        = aws_s3_bucket.react_app.id
+  }
 }
