d552897e92f7127ede246464bf64e483a3fac96f
Chris <chris_carr@jake.ccarr.com>
2021-12-29 07:39:40 -0500

    Revert

--- react/admin/src/List.js
+++ react/admin/src/List.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
-import PoolData from './PoolData.js';
-const UserPool = new CognitoUserPool(PoolData);
 // TODO: move this to a configuration file
 const invokeUrl = 'https://osg06j8v6e.execute-api.us-east-1.amazonaws.com/prod';
 
@@ -16,32 +13,9 @@ class List extends React.Component {
   }
 
   async getList() {
-    // authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
-    //   var cognitoUser = userPool.getCurrentUser();
-
-    //   if (cognitoUser) {
-    //       cognitoUser.getSession(function sessionCallback(err, session) {
-    //           if (err) {
-    //               reject(err);
-    //           } else if (!session.isValid()) {
-    //               resolve(null);
-    //           } else {
-    //               resolve(session.getIdToken().getJwtToken());
-    //           }
-    //       });
-    //   } else {
-    //       resolve(null);
-    //   }
-    // });
-    const user = UserPool.getCurrentUser();
-    const token = await user.getSession((err, session) => {
-      return session.getIdToken().getJwtToken();
-    });
-    console.log(token);
-    console.log(this.state.token);
     const response = await axios.get(invokeUrl + '/list', {
       headers: {
-        Authorization: token
+        Authorization: this.state.token
       }
     });
     alert(response);
