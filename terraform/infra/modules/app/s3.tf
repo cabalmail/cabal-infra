2f93dcffdf68465a43338492ac433642c6a6e538
Chris <chris_carr@jake.ccarr.com>
2021-12-30 18:23:04 -0500

    Supply missing var

--- terraform/infra/modules/app/s3.tf
+++ terraform/infra/modules/app/s3.tf
@@ -33,15 +33,17 @@ resource "aws_s3_bucket_object" "website_config" {
     pool_id        = var.user_pool_id,
     pool_client_id = var.user_pool_client_id,
     region         = var.region,
-    invoke_url     = aws_api_gateway_deployment.deployment.invoke_url
-    domains        = {for domain in var.domains : domain.domain => domain.zone_id}
+    invoke_url     = aws_api_gateway_deployment.deployment.invoke_url,
+    domains        = {for domain in var.domains : domain.domain => domain.zone_id},
+    control_domain = var.control_domain
   })
   etag         = md5(templatefile("${path.module}/templates/config.js", {
       pool_id        = var.user_pool_id,
       pool_client_id = var.user_pool_client_id,
       region         = var.region,
-      invoke_url     = aws_api_gateway_deployment.deployment.invoke_url
-      domains        = {for domain in var.domains : domain.domain => domain.zone_id}
+      invoke_url     = aws_api_gateway_deployment.deployment.invoke_url,
+      domains        = {for domain in var.domains : domain.domain => domain.zone_id},
+      control_domain = var.control_domain
     })
   )
 }
