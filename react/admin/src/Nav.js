c161dfea49f001b6daa3e8ac7e04e9017dea4b81
Chris <chris_carr@jake.ccarr.com>
2021-12-30 11:14:26 -0500

    General cleanup and a little semantic ui for menu

--- react/admin/src/Nav.js
+++ react/admin/src/Nav.js
@@ -6,21 +6,21 @@ class Nav extends React.Component {
     const { loggedIn, onClick } = this.props;
     console.log(loggedIn);
     return (
-      <div className={`nav logged-${loggedIn ? 'in' : 'out'}`}>
+      <div className={`ui pointing menu nav logged-${loggedIn ? 'in' : 'out'}`}>
         <ul className="nav">
-          <li id="list">
+          <li id="list" className={`item${this.props.view == "List" ? " selected" : ""}`}>
             <button name="List" onClick={onClick}>List Addresses</button>
           </li>
-          <li id="request">
+          <li id="request" className={`item${this.props.view == "Request" ? " selected" : ""}`}>
             <button name="Request" onClick={onClick}>New Address</button>
           </li>
-          <li id="login">
+          <li id="login" className={`item${this.props.view == "Login" ? " selected" : ""}`}>
             <button name="Login" onClick={onClick}>Log in</button>
           </li>
-          <li id="signup">
+          <li id="signup" className={`item${this.props.view == "SignUp" ? " selected" : ""}`}>
             <button name="SignUp" onClick={onClick}>Sign up</button>
           </li>
-            <li id="logout">
+            <li id="logout" className="item">
             <button name="LogOut" onClick={onClick}>Log out</button>
           </li>
         </ul>
