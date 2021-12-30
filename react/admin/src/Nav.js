ed1ed672d242ee610b7e8d7f0a9d2fbe0ee8062b
Chris <chris_carr@jake.ccarr.com>
2021-12-30 11:24:17 -0500

    More work on nav

--- react/admin/src/Nav.js
+++ react/admin/src/Nav.js
@@ -3,27 +3,35 @@ import './Nav.css'
 
 class Nav extends React.Component {
   render() {
-    const { loggedIn, onClick } = this.props;
+    const { loggedIn, onClick, view } = this.props;
     console.log(loggedIn);
     return (
       <div className={`ui pointing menu nav logged-${loggedIn ? 'in' : 'out'}`}>
-        <ul className="nav">
-          <li id="list" className={`item${this.props.view === "List" ? " selected" : ""}`}>
-            <button name="List" onClick={onClick}>List Addresses</button>
-          </li>
-          <li id="request" className={`item${this.props.view === "Request" ? " selected" : ""}`}>
-            <button name="Request" onClick={onClick}>New Address</button>
-          </li>
-          <li id="login" className={`item${this.props.view === "Login" ? " selected" : ""}`}>
-            <button name="Login" onClick={onClick}>Log in</button>
-          </li>
-          <li id="signup" className={`item${this.props.view === "SignUp" ? " selected" : ""}`}>
-            <button name="SignUp" onClick={onClick}>Sign up</button>
-          </li>
-            <li id="logout" className="item">
-            <button name="LogOut" onClick={onClick}>Log out</button>
-          </li>
-        </ul>
+        <a
+          id="list"
+          className={`item${view === "List" ? " selected" : ""}`}
+          name="List"
+          onClick={onClick}
+        >List Addresses</a>
+        <a
+          id="request"
+          className={`item${view === "Request" ? " selected" : ""}`}
+          name="Request"
+          onClick={onClick}
+        >New Address</a>
+        <a
+          id="login"
+          className={`item${view === "Login" ? " selected" : ""}`}
+          name="Login"
+          onClick={onClick}
+        >Log in</a>
+        <a
+          id="signup"
+          className={`item${view === "SignUp" ? " selected" : ""}`}
+          name="SignUp"
+          onClick={onClick}
+        >Sign up</a>
+        <a id="logout" className="item" name="LogOut" onClick={onClick}>Log out</a>
       </div>
     );
   }
