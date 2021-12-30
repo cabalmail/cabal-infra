d4be90dd1155f9806d90f4e66cbb4cd7f54e11c5
Chris <chris_carr@jake.ccarr.com>
2021-12-30 18:37:46 -0500

    Reformat config as json

--- terraform/infra/modules/app/templates/config.js
+++ terraform/infra/modules/app/templates/config.js
@@ -1,10 +1,12 @@
-const control_domain = "${control_domain}";
-const domains = ${jsonencode(domains)};
-const cognitoConfig = {
-  invokeUrl: "${invoke_url}",
-  region: "${region}",
-  poolData = {
-    UserPoolId: "${pool_id}",
-    ClientId: "${pool_client_id}"
+{
+  "control_domain": "${control_domain}",
+  "domains": ${jsonencode(domains)},
+  "cognitoConfig": {
+    "invokeUrl": "${invoke_url}",
+    "region": "${region}",
+    "poolData": {
+      "UserPoolId": "${pool_id}",
+      "ClientId": "${pool_client_id}"
+    }
   }
-};
+}
