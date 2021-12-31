5a9776430b7e6ac6e061351b1b50006fb55da1f1
Chris <chris_carr@jake.ccarr.com>
2021-12-30 19:09:32 -0500

    invokeUrl shouldn't be part of cognito config

--- terraform/infra/modules/app/templates/config.js
+++ terraform/infra/modules/app/templates/config.js
@@ -1,8 +1,8 @@
 {
   "control_domain": "${control_domain}",
   "domains": ${jsonencode(domains)},
-  "cognitoConfig": {
-    "invokeUrl": "${invoke_url}",
+  "invokeUrl": "${invoke_url}",
+    "cognitoConfig": {
     "region": "${region}",
     "poolData": {
       "UserPoolId": "${pool_id}",
