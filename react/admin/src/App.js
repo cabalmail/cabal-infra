aac1fa2160c45ff5567d1356605430ccb8a4c59a
Chris <chris_carr@jake.ccarr.com>
2021-12-30 19:00:03 -0500

    Use arrow function to bind this to class

--- react/admin/src/App.js
+++ react/admin/src/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
     this.getConfig();
   }
 
-  async getConfig() {
+  getConfig = async => {
     const response = await axios.get('/config.js').catch( (err) => {
       if (err.response) {
         console.log("Error in response while retrieving configuration", err.response);
