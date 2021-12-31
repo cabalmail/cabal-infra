cac334a82829fd191bf0be5be3d48646bdbc54c1
Chris <chris_carr@jake.ccarr.com>
2021-12-30 22:13:56 -0500

    Reformat domains in config

--- terraform/infra/modules/app/s3.tf
+++ terraform/infra/modules/app/s3.tf
@@ -34,7 +34,7 @@ resource "aws_s3_bucket_object" "website_config" {
     pool_client_id = var.user_pool_client_id,
     region         = var.region,
     invoke_url     = "${aws_api_gateway_deployment.deployment.invoke_url}prod",
-    domains        = {for domain in var.domains : domain.domain => domain.zone_id},
+    domains        = var.domains,
     control_domain = var.control_domain
   })
   etag         = md5(templatefile("${path.module}/templates/config.js", {
@@ -42,7 +42,7 @@ resource "aws_s3_bucket_object" "website_config" {
       pool_client_id = var.user_pool_client_id,
       region         = var.region,
       invoke_url     = "${aws_api_gateway_deployment.deployment.invoke_url}prod",
-      domains        = {for domain in var.domains : domain.domain => domain.zone_id},
+      domains        = var.domains,
       control_domain = var.control_domain
     })
   )
