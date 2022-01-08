dce8a3eacb0c434550f1b56cbd14b5a01c886c73
Chris <chris_carr@jake.ccarr.com>
2022-01-08 17:13:23 -0500

    Simplify address list and add copy button

--- react/admin/src/AddressList.js
+++ react/admin/src/AddressList.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import Address from './Address';
 
 class AddressList extends React.Component {
 
+  copy = (e) => {
+    e.preventDefault();
+    const address = e.target.value;
+    navigator.clipboard.writeText(address);
+    this.props.setMessage(`The address ${address} has been copied to your clipboard.`);
+  }
+
   render() {
-    const addresses = this.props.addresses.map(a => <Address key={a.address} address={a.address}  setMessage={this.props.setMessage} />);
+    const addresses = this.props.addresses.map(a => {
+      return (
+        <li key={a.address} className="address">
+          <span>{a.address}</span>
+          <button onClick={this.copy} value={a.address}>copy</button>
+        </li>
+      )
+    });
     return (
       <ul className="address-list">
         {addresses}
