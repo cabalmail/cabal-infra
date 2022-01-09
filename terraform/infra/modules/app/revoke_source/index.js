beb8b70fa5af62ba0e6071586e429b7ce5a3ad74
Chris <chris_carr@jake.ccarr.com>
2022-01-09 09:58:26 -0500

    Check all return values

--- terraform/infra/modules/app/revoke_source/index.js
+++ terraform/infra/modules/app/revoke_source/index.js
@@ -26,70 +26,77 @@ exports.handler = (event, context, callback) => {
 
     const lines = public_key.split(/\r?\n/);
     const publicKeyFlattened = lines[1] + lines[2] + lines[3];
-    revokeAddress(address).then(v => {
-      var params = {
-        "HostedZoneId": zone_id,
-        "ChangeBatch": {
-          "Changes": [
-            {
-              "Action": "DELETE",
-              "ResourceRecordSet": {
-                "Name": subdomain + '.' + tld,
-                "TTL": 3600,
-                "Type": "MX",
-                "ResourceRecords": [
-                  {
-                    Value: '10 smtp-in.' + control_domain
-                  }
-                ]
-              }
-            },
-            {
-              "Action": "DELETE",
-              "ResourceRecordSet": {
-                "Name": 'cabal._domainkey.' + subdomain + '.' + tld,
-                "TTL": 3600,
-                "Type": "TXT",
-                "ResourceRecords": [
-                  {
-                    Value: '"' + publicKeyFlattened + '"'
-                  }
-                ]
-              }
-            },
-            {
-              "Action": "DELETE",
-              "ResourceRecordSet": {
-                "Name": subdomain + '.' + tld,
-                "TTL": 3600,
-                "Type": "TXT",
-                "ResourceRecords": [
-                  {
-                    Value: '"v=spf1 include:' + control_domain + ' ~all"'
-                  }
-                ]
-              }
+    const params = {
+      "HostedZoneId": zone_id,
+      "ChangeBatch": {
+        "Changes": [
+          {
+            "Action": "DELETE",
+            "ResourceRecordSet": {
+              "Name": subdomain + '.' + tld,
+              "TTL": 3600,
+              "Type": "MX",
+              "ResourceRecords": [
+                {
+                  Value: '10 smtp-in.' + control_domain
+                }
+              ]
             }
-          ]
-        }
-      };
-
-      route53.changeResourceRecordSets(params, function(err,data) {
-        console.log(err,data);
-      });
-      callback(null, {
-          statusCode: 202,
-          body: JSON.stringify({
-            "status": "success",
-            "address": address
-          }),
-          headers: {
-              'Access-Control-Allow-Origin': '*',
           },
+          {
+            "Action": "DELETE",
+            "ResourceRecordSet": {
+              "Name": 'cabal._domainkey.' + subdomain + '.' + tld,
+              "TTL": 3600,
+              "Type": "TXT",
+              "ResourceRecords": [
+                {
+                  Value: '"' + publicKeyFlattened + '"'
+                }
+              ]
+            }
+          },
+          {
+            "Action": "DELETE",
+            "ResourceRecordSet": {
+              "Name": subdomain + '.' + tld,
+              "TTL": 3600,
+              "Type": "TXT",
+              "ResourceRecords": [
+                {
+                  Value: '"v=spf1 include:' + control_domain + ' ~all"'
+                }
+              ]
+            }
+          }
+        ]
+      }
+    };
+
+    const r53_req = route53.changeResourceRecordSets(params, function(err,data) {
+      if (err) {
+        console.error("r53 error", err);
+        errorResponse(err, context.awsRequestId, callback);
+      }
+    }).promise();
+    const ddb_req = revokeAddress(address);
+    Promise.all([ddb_req, r53_req]).then(values => {
+      callback(null, {
+        statusCode: 202,
+        body: JSON.stringify({
+          "status": "success",
+          "address": address
+        }),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+      }, reason => {
+        console.error("rejected", reason);
+        throw "Rejected";
       });
     }).catch((err) => {
         console.error(err);
-        errorResponse(err.message, context.awsRequestId, callback)
+        errorResponse(err.message, context.awsRequestId, callback);
     });
 };
 
