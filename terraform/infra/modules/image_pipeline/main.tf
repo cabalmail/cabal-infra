6d09a9e0497e016999e98bbdaacfc2eaadfe7796
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-02-15 14:01:24 -0500

    Update documentation to reflect ability to proceed with codepipeline

--- terraform/infra/modules/image_pipeline/main.tf
+++ terraform/infra/modules/image_pipeline/main.tf
@@ -2,7 +2,7 @@
 *
 * # Status
 *
-* On hold pending resolution of [issue #16839](https://github.com/hashicorp/terraform-provider-aws/issues/16839) which will add Terraform support for building docker container images.
+* ~~On hold pending resolution of [issue #16839](https://github.com/hashicorp/terraform-provider-aws/issues/16839) which will add Terraform support for building docker container images.~~ Update: resolved in 4.1.0.
 *
 * Code to date adapted from [this sample](https://github.com/aws-samples/amazon-ec2-image-builder-samples/blob/master/CloudFormation/Docker/amazon-linux-2-with-helloworld/amazon-linux-2-container-image.yml).
 *
