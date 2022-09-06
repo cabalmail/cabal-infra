c39225b12543ec3dfdfc9bf33f1cb34482c1271a
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-09-05 20:24:55 -0400

    Remove cognito verification

--- terraform/dns/modules/user_pool/main.tf
+++ terraform/dns/modules/user_pool/main.tf
@@ -4,11 +4,6 @@
 
 resource "aws_cognito_user_pool" "users" {
   name                     = "cabal"
-  auto_verified_attributes = [ "phone_number" ]
-  sms_configuration {
-    external_id    = "${data.aws_caller_identity.current.account_id}_DgEGa1t3qz"
-    sns_caller_arn = aws_iam_role.users.arn
-  }
   account_recovery_setting {
     recovery_mechanism {
       name     = "verified_phone_number"
