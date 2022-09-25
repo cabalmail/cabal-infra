871ab85d7805baf871e561021404eb878af1f4ee
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-09-25 08:12:48 -0400

    Remove old refs

--- terraform/infra/modules/app/modules/call/cors.tf
+++ terraform/infra/modules/app/modules/call/cors.tf
@@ -1,6 +1,6 @@
 module "cors" {
-  source = "squidfunk/api-gateway-enable-cors/aws"
-  version = "0.3.3"
+  source          = "squidfunk/api-gateway-enable-cors/aws"
+  version         = "0.3.3"
 
   api_id          = var.gateway_id
   api_resource_id = aws_api_gateway_resource.api_call.id
