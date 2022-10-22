a654c8dc868c81de956cd4f8fe9cd3e7997a9f9e
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-10-21 20:31:41 -0400

    Compute from data source output

--- terraform/infra/modules/app/modules/call/modules/python_lambda/main.tf
+++ terraform/infra/modules/app/modules/call/modules/python_lambda/main.tf
@@ -145,7 +145,7 @@ RUNPOLICY
 #tfsec:ignore:aws-lambda-enable-tracing
 resource "aws_lambda_function" "api_call" {
   filename         = data.archive_file.python_code.output_path
-  source_code_hash = filemd5(data.archive_file.python_code.output_path)
+  source_code_hash = data.archive_file.python_code.output_base64sha256
   function_name    = var.name
   role             = aws_iam_role.lambda.arn
   handler          = "function.handler"
