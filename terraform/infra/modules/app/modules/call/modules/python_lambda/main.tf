26feef8bdacae2353670a6d17fb5744cd7298c66
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-10-22 08:52:34 -0400

    Try just bash

--- terraform/infra/modules/app/modules/call/modules/python_lambda/main.tf
+++ terraform/infra/modules/app/modules/call/modules/python_lambda/main.tf
@@ -9,7 +9,7 @@ locals {
 
 resource "null_resource" "python_build" {
   provisioner "local-exec" {
-    interpreter = ["/usr/bin/bash"]
+    interpreter = ["bash"]
     command     = <<-EOT
       set -e
       cp ${local.path}/requirements.txt ${local.build_path}/
