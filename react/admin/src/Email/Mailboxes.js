bbd0958853286b7509d2d7a7604ae47dfb54912a
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-10-22 13:35:29 -0400

    First try at getting mailboxes from React and displaying them to end user.

--- react/admin/src/Email/Mailboxes.js
+++ react/admin/src/Email/Mailboxes.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
 class Mailboxes extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mailboxes: ""
+    };
+  }
+
+  componentDidMount() {
+    const response = this.getList();
+    response.then(data => {
+      this.setState({ mailboxes: data });
+    });
+  }
+
+  getList = async (e) => {
+    const response = await axios.post('/list_mailboxes', {
+      baseURL: this.props.api_url,
+      headers: {
+        'Authorization': this.props.token
+      },
+      timeout: 10000,
+      params: {
+        user: this.props.userName,
+        password: this.props.password
+      }
+    });
+    return response;
+  }
+
   render() {
-    return <div>Mailboxes</div>;
+    return (
+      <>
+        <div>Mailboxes</div>
+        <div>{this.state.mailboxes}</div>
+      </>
+    );
   }
 }
 
