ba443af4b2a7ff413d12fd5650086042c56b9fa0
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-10-21 20:02:43 -0400

    Separate node and python modules

--- terraform/infra/modules/app/modules/call/lambda.tf
+++ terraform/infra/modules/app/modules/call/lambda.tf
@@ -1,5 +1,18 @@
-module "cabal_lambda" {
-  source         = var.type == "python" ? "./modules/python_lambda" : "./modules/node_lambda"
+module "cabal_python_lambda" {
+  count          = var.type == "python" ? 1 : 0
+  source         = "./modules/python_lambda"
+  name           = var.name
+  runtime        = var.runtime
+  region         = var.region
+  account        = var.account
+  control_domain = var.control_domain
+  relay_ips      = var.relay_ips
+  domains        = var.domains
+}
+
+module "cabal_node_lambda" {
+  count          = var.type == "node" ? 1 : 0
+  source         = "./modules/node_lambda"
   name           = var.name
   runtime        = var.runtime
   region         = var.region
