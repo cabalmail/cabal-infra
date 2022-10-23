f683470afcd9c841c3bebb449832813d88b27997
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-10-23 10:26:51 -0400

    Safari doesn't honor * for allowed cors headers

--- terraform/infra/modules/app/modules/call/cors.tf
+++ terraform/infra/modules/app/modules/call/cors.tf
@@ -4,5 +4,14 @@ module "cors" {
 
   api_id          = var.gateway_id
   api_resource_id = aws_api_gateway_resource.api_call.id
-  allow_headers   = ["*"]
+  allow_headers   = [
+    "Authorization",
+    "Content-Type",
+    "X-Amz-Date",
+    "X-Amz-Security-Token",
+    "X-Api-Key",
+    "origin",
+    "Referer",
+    "User-Agent"
+  ]
 }
