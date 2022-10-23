8e9c238f276af12738e147dbaada381bfc8b78c2
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-10-22 20:11:40 -0400

    Return correct value for API gateway

--- lambda/python/list_mailboxes/function.py
+++ lambda/python/list_mailboxes/function.py
@@ -5,13 +5,15 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-client = IMAPClient(host="imap.${control_domain}", use_uid=True, ssl=True)
-
 def handler(event, context):
-  logger.info(json.dumps(event))
-  logger.info(event['user'])
-  client.login(event['user'], event['password'])
+  client = IMAPClient(host="imap.${control_domain}", use_uid=True, ssl=True)
+  logger.info(event['body'])
+  body = json.loads(event['body'])
+  client.login(body['user'], body['password'])
   response = client.list_folders()
   client.logout()
   logger.info(response)
-  return response
+  return {
+    "statusCode": 200,
+    "body": response
+  }
