9c710ee0483212af6567fbcf9e022b5604538ce2
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-10-30 16:52:45 -0400

    Comment out cors config

--- terraform/infra/modules/app/modules/call/cors.tf
+++ terraform/infra/modules/app/modules/call/cors.tf
@@ -1,27 +1,28 @@
-module "cors" {
-  source          = "squidfunk/api-gateway-enable-cors/aws"
-  version         = "0.3.3"
+# module "cors" {
+#   source          = "squidfunk/api-gateway-enable-cors/aws"
+#   version         = "0.3.3"
 
-  api_id          = var.gateway_id
-  api_resource_id = aws_api_gateway_resource.api_call.id
-  allow_headers   = [
-    "Authorization",
-    "Content-Type",
-    "X-Amz-Date",
-    "X-Amz-Security-Token",
-    "X-Api-Key",
-    "origin",
-    "Referer",
-    "User-Agent",
-    "X-Requested-With",
-    "Accept-Encoding",
-    "Content-Length",
-    "DNT",
-    "If-Modified-Since",
-    "Cache-Control",
-    "Range"
-  ]
-}
+#   api_id          = var.gateway_id
+#   api_resource_id = aws_api_gateway_resource.api_call.id
+#   allow_headers   = [
+#     "Authorization",
+#     "Content-Type",
+#     "X-Amz-Date",
+#     "X-Amz-Security-Token",
+#     "X-Api-Key",
+#     "origin",
+#     "Referer",
+#     "User-Agent",
+#     "X-Requested-With",
+#     "Accept-Encoding",
+#     "Content-Length",
+#     "DNT",
+#     "If-Modified-Since",
+#     "Cache-Control",
+#     "Range",
+#     "If-None-Match"
+#   ]
+# }
 #,,Authorization, Content-Language, Accept, Accept-Language
 
 # CORS Safe: Accept Accept-Language Content-Language Content-Type
