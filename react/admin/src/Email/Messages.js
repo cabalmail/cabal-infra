40545e059f94418266b04e0297a021f392b8c64d
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-11-20 19:04:20 -0500

    Ditch lazy load

--- react/admin/src/Email/Messages.js
+++ react/admin/src/Email/Messages.js
@@ -4,7 +4,6 @@
 
 import React from 'react';
 import axios from 'axios';
-import LazyLoad from 'react-lazyload';
 import Envelopes from './Envelopes.js';
 
 // see https://www.rfc-editor.org/rfc/rfc5256.html
