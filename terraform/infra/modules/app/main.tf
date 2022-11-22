5f0b1d367bafdbe09bb5c5105823a5bb4b70ec99
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-11-22 12:31:22 -0500

    Trigger API redeployment on change to envelopes call

--- terraform/infra/modules/app/main.tf
+++ terraform/infra/modules/app/main.tf
@@ -142,7 +142,8 @@ resource "aws_api_gateway_deployment" "deployment" {
       module.cabal_new_method.hash_key,
       module.cabal_revoke_method.hash_key,
       module.cabal_list_mailboxes_method.hash_key,
-      module.cabal_list_messages_method.hash_key
+      module.cabal_list_messages_method.hash_key,
+      module.cabal_list_envelopes_method.hash_key
     ]))
   }
   lifecycle {
