9375327f13ee7c7212aa6cd6c700644ff16c3f05
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-11-23 22:06:25 -0500

    Invert render logic in overlay

--- react/admin/src/Email/MessageOverlay.js
+++ react/admin/src/Email/MessageOverlay.js
@@ -9,27 +9,26 @@ class MessageOverlay extends React.Component {
   }
 
   render() {
-    if (!this.props.visible) {
-      return <div className="message_overlay overlay_hidden"></div>;
+    if (this.props.visible) {
+      return (
+        <div className="message_overlay">
+          <button onClick={this.hide}>❌</button>
+          <dl>
+            <dt>To</dt>
+            <dd>{this.props.envelope.to}</dd>
+            <dt>From</dt>
+            <dd>{this.props.envelope.from.join("; ")}</dd>
+            <dt>Received</dt>
+            <dd>{this.props.envelope.date}</dd>
+            <dt>Subject</dt>
+            <dd>{this.props.envelope.subject}</dd>
+          </dl>
+          <div className="body">Not implamented</div>
+        </div>
+      );
     }
-    return (
-      <div className="message_overlay">
-        <button onClick={this.hide}>❌</button>
-        <dl>
-          <dt>To</dt>
-          <dd>{this.props.envelope.to}</dd>
-          <dt>From</dt>
-          <dd>{this.props.envelope.from.join("; ")}</dd>
-          <dt>Received</dt>
-          <dd>{this.props.envelope.date}</dd>
-          <dt>Subject</dt>
-          <dd>{this.props.envelope.subject}</dd>
-        </dl>
-        <div className="body">Not implamented</div>
-      </div>
-    );
+    return <div className="message_overlay overlay_hidden"></div>;
   }
-
 }
 
 export default MessageOverlay;
