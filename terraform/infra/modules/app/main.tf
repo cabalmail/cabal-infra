92ff99e7362ec5054d29b4b0bcc92d90682f5960
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-11-26 18:29:02 -0500

    Redeploy API on update of list_attachments

--- terraform/infra/modules/app/main.tf
+++ terraform/infra/modules/app/main.tf
@@ -184,7 +184,8 @@ resource "aws_api_gateway_deployment" "deployment" {
       module.cabal_list_mailboxes_method.hash_key,
       module.cabal_list_messages_method.hash_key,
       module.cabal_list_envelopes_method.hash_key,
-      module.cabal_fetch_message_method.hash_key
+      module.cabal_fetch_message_method.hash_key,
+      module.cabal_list_attachments_method.hash_key
     ]))
   }
   lifecycle {
