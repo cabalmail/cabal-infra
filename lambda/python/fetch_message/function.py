ec09feb77b94618bd4670b8eea6975f468ca6500
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-11-26 19:47:33 -0500

    Handle large messages

--- lambda/python/fetch_message/function.py
+++ lambda/python/fetch_message/function.py
@@ -43,6 +43,8 @@ def handler(event, _context):
         body_plain_decoded = body_plain.decode()
     except AttributeError:
         body_plain_decoded = body_plain.__str__()
+    if len(message.__str__()) > 1000000:
+        message = "Raw message too large to display"
     return {
         "statusCode": 200,
         "body": json.dumps({
