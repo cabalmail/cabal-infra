e9fc60cad343f2199202bacdbc50d89f692a6d47
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-11-28 12:40:44 -0500

    Remove unused ECR resources

--- terraform/dns/outputs.tf
+++ terraform/dns/outputs.tf
@@ -5,11 +5,3 @@ output "control_domain_name_servers" {
 output "aws_s3_bucket" {
   value = aws_s3_bucket.react_app.id
 }
-
-output "aws_ecr_repository_registry_id" {
-  value = aws_ecr_repository.container_repo.registry_id
-}
-
-output "aws_ecr_repository_repository_url" {
-  value = aws_ecr_repository.container_repo.repository_url
-}
