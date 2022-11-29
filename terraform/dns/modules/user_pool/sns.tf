82825b990e9647e9a6d40995210d51517634cbf9
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-11-29 14:09:27 -0500

    Associate policy with user pool

--- terraform/dns/modules/user_pool/sns.tf
+++ terraform/dns/modules/user_pool/sns.tf
@@ -3,12 +3,35 @@ resource "aws_iam_role" "users" {
   assume_role_policy = data.aws_iam_policy_document.users.json
 }
 
-resource "aws_iam_policy" "users" {
+resource "aws_iam_policy" "sns" {
   name   = "cabal_sns_role_policy"
   policy = data.aws_iam_policy_document.sns_users.json
 }
 
+data "aws_iam_policy_document" "cognito_to_s3" {
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.react_app.arn]
+    condition {
+      test     = "StringLike"
+      variable = "s3:prefix"
+      values   = ["message-cache/${"$"}{cognito-identity.amazonaws.com:sub}/*"]
+    }
+  }
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.react_app.id}/message-cache/${"$"}{cognito-identity.amazonaws.com:sub}/*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "sns" {
+  role       = aws_iam_role.users.name
+  policy_arn = aws_iam_policy.sns.arn
+}
+
 resource "aws_iam_role_policy_attachment" "users" {
   role       = aws_iam_role.users.name
-  policy_arn = aws_iam_policy.users.arn
+  policy_arn = aws_iam_policy_document.cognito_to_s3.arn
 }
