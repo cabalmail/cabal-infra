832799bef06995817b535db76ee194af9b68cc8a
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-03 13:02:27 -0500

    Git origin domain for s3 bucket

--- terraform/infra/modules/app/variables.tf
+++ terraform/infra/modules/app/variables.tf
@@ -2,6 +2,10 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
+data "aws_s3_bucket" "this" {
+  bucket = "admin.${var.control_domain}"
+}
+
 variable "user_pool_id" {
   type = string
   description = "ID of the Cognito user pool."
