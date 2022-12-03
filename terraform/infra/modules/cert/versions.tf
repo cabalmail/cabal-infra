143b35537545f92ce6b85eb4e6637e20184b4c23
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-03 09:51:31 -0500

    Specify tls provider in cert module

--- terraform/infra/modules/cert/versions.tf
+++ terraform/infra/modules/cert/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "vancluever/acme"
       version = "2.2.0"
     }
+    tls = {
+      source = "hashicorp/tls"
+      version = ">= 4.0.0"
+    }
   }
 
   required_version = ">= 1.1.2"
