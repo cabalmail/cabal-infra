fba1aea90150eb17454418d4d74bd98035657ccc
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-03 11:29:03 -0500

    Pass bucket value to asg module

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -110,6 +110,7 @@ module "imap" {
   user_pool_id     = local.user_pool_id
   scale            = var.imap_scale
   chef_license     = var.chef_license
+  bucket           = var.bucket.bucket
   depends_on       = [ module.cert ]
 }
 
@@ -134,6 +135,7 @@ module "smtp_in" {
   user_pool_arn    = local.user_pool_arn
   scale            = var.smtpin_scale
   chef_license     = var.chef_license
+  bucket           = var.bucket.bucket
   depends_on       = [ module.cert ]
 }
 
@@ -161,6 +163,7 @@ module "smtp_out" {
   user_pool_arn    = local.user_pool_arn
   scale            = var.smtpout_scale
   chef_license     = var.chef_license
+  bucket           = var.bucket.bucket
   depends_on       = [ module.cert ]
 }
 
