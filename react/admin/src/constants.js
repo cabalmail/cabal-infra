252c699a403a089028108f1cd36ff5437dd2bc38
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-14 01:14:36 -0500

    Export

--- react/admin/src/constants.js
+++ react/admin/src/constants.js
@@ -1,31 +1,31 @@
-const ASC = {
+export const ASC = {
   imap: "",
   css: "ascending",
   description: "Ascending order (smallest/first to largest/last)"
 };
-const DESC = {
+export const DESC = {
   imap: "REVERSE ",
   css: "descending",
   description: "Descending order (largest/last to smallest/first)"
 };
-const ARRIVAL = {
+export const ARRIVAL = {
   imap: "ARRIVAL",
   css: "arrival",
   description: "Date Received"
 };
-const FROM = {
+export const FROM = {
   imap: "FROM",
   css: "from",
   description: "From address"
 };
-const SUBJECT = {
+export const SUBJECT = {
   imap: "SUBJECT",
   css: "subject",
   description: "Subject"
 };
-const TO = {
+export const TO = {
   imap: "TO",
   css: "to",
   description: "Recipient"
 };
-const PAGE_SIZE = 50;
+export const PAGE_SIZE = 50;
