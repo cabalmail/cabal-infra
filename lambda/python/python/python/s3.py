070119c3792f4908b275541018e0c81e47b6d73b
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-17 23:13:08 -0500

    s3 resource

--- lambda/python/python/python/s3.py
+++ lambda/python/python/python/s3.py
@@ -2,8 +2,8 @@ import boto3
 import botocore
 from botocore.exceptions import ClientError
 import io
-
-s3 = boto3.client("s3",
+s3r = boto3.resource("s3")
+s3c = boto3.client("s3",
                   region_name="us-east-1",
                   config=boto3.session.Config(signature_version='s3v4'))
                   
@@ -13,7 +13,7 @@ def upload_object(bucket, key, content_type, obj):
         f.write(obj)
         f.seek(0)
         try:
-            s3.upload_fileobj(f, bucket, key, ExtraArgs={'ContentType': content_type})
+            s3c.upload_fileobj(f, bucket, key, ExtraArgs={'ContentType': content_type})
         except ClientError as e:
             logging.error(e)
             return False
@@ -26,7 +26,7 @@ def sign_url(bucket, key, expiration=86400):
         'Key': key
     }
     try:
-        url = s3.generate_presigned_url('get_object',
+        url = s3c.generate_presigned_url('get_object',
                                         Params=params,
                                         ExpiresIn=expiration)
     except Exception as e:
@@ -37,7 +37,7 @@ def sign_url(bucket, key, expiration=86400):
 def key_exists(bucket, key):
     '''checks wither a key exists in a given bucket'''
     try:
-        s3.Object(bucket, key).load()
+        s3r.Object(bucket, key).load()
     except ClientError as e:
         if e.response['Error']['Code'] == "404":
             return False
