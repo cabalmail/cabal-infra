e041a81de4c63cbc5825279fdfdc5ae6d26cacd3
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-20 14:51:00 -0500

    Fix lambda permissions

--- terraform/infra/modules/app/modules/call/lambda.tf
+++ terraform/infra/modules/app/modules/call/lambda.tf
@@ -59,7 +59,7 @@ resource "aws_iam_role_policy" "lambda" {
         {
             "Effect": "Allow",
             "Action": [
-                "ssm:GetParameters"
+                "ssm:GetParameter"
             ],
             "Resource": "arn:aws:ssm:${var.region}:${var.account}:parameter/cabal/master_password"
         },
