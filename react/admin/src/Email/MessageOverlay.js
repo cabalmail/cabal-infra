4963ddbc790e3da0b832b96ca79c5750c596f31f
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-20 20:30:19 -0500

    Style iframe

--- react/admin/src/Email/MessageOverlay.js
+++ react/admin/src/Email/MessageOverlay.js
@@ -159,7 +159,7 @@ class MessageOverlay extends React.Component {
         );
       case "raw":
         return (
-          <iframe src={this.state.message_raw_url} title="Raw message" className="message_raw" />
+          <iframe src={this.state.message_raw_url} title="Raw message" className="message_raw" ></iframe>
         );
       case "attachments":
         const attachments = this.state.attachments.map(a => {
