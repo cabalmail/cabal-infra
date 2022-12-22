3af0c964aa2652137eb3ec4dac7b688ecc44fca6
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-21 22:22:13 -0500

    PUT data is conveyed in the body

--- lambda/python/set_flag/function.py
+++ lambda/python/set_flag/function.py
@@ -4,15 +4,15 @@ from s3 import get_imap_client
 
 def handler(event, _context):
     '''Sets IMAP flags on messages for a user given a folder and list of message ids'''
-    qs = event['queryStringParameters']
-    ids = json.loads(qs['ids'])
+    body = event['body']
+    ids = json.loads(body['ids'])
     user = event['requestContext']['authorizer']['claims']['cognito:username'];
-    client = get_imap_client(qs['host'], user, qs['folder'])
-    if qs['op'] == 'set':
-        client.set_flags(ids, qs['flag'], True)
+    client = get_imap_client(body['host'], user, body['folder'])
+    if body['op'] == 'set':
+        client.set_flags(ids, body['flag'], True)
     else:
-        client.remove_flags(ids, qs['flag'], True)
-    response = client.sort(f"{qs['sort_order']}{qs['sort_field']}", [b'NOT', b'DELETED'])
+        client.remove_flags(ids, body['flag'], True)
+    response = client.sort(f"{body['sort_order']}{body['sort_field']}", [b'NOT', b'DELETED'])
     client.logout()
     return {
         "statusCode": 200,
