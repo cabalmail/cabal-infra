383d10d5e7eb57118c9a5f9b49ccfaa37e7060cf
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-22 12:32:41 -0500

    Different decoding approach

--- lambda/python/python/python/s3.py
+++ lambda/python/python/python/s3.py
@@ -33,7 +33,7 @@ def get_message(host, user, folder, id):
     else:
         client = get_imap_client(host, user, folder)
         client.select_folder(folder)
-        email_body_raw = client.fetch([id],[b"RFC822"])[id][b"RFC822"]
+        email_body_raw = client.fetch([id],[b"RFC822"])[id].__bytes__()
         client.logout()
         upload_object(bucket, key, "text/plain", email_body_raw)
     message = email.message_from_bytes(email_body_raw, policy=default_policy)
