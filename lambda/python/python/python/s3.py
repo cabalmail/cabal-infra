69428bd28021dc69f7c913d0a7f6d09e4712eff2
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-22 12:44:46 -0500

    Debugging

--- lambda/python/python/python/s3.py
+++ lambda/python/python/python/s3.py
@@ -33,7 +33,9 @@ def get_message(host, user, folder, id):
     else:
         client = get_imap_client(host, user, folder)
         client.select_folder(folder)
-        email_body_raw = client.fetch([id],[b"RFC822"])[id].__bytes__()
+        message_array = client.fetch([id],[b'RFC822'])
+        message_obj = message_array[id]
+        email_body_raw = message_obj[b'RFC822']
         client.logout()
         upload_object(bucket, key, "text/plain", email_body_raw)
     message = email.message_from_bytes(email_body_raw, policy=default_policy)
