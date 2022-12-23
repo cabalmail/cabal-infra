6e58bdaf78874880b33bb778d81af04fe2fd6983
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-23 11:06:42 -0500

    Buttons, form, and styling

--- react/admin/src/Folders/index.js
+++ react/admin/src/Folders/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import './Folders.css';
 
 /**
  * Fetches folders for current users and displays them
@@ -10,7 +11,8 @@ class Folders extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      folders: []
+      folders: [],
+      new_folder: ''
     };
   }
 
@@ -43,17 +45,55 @@ class Folders extends React.Component {
     this.props.setFolder(e.target.value);
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form not implemeted yet.");
+  }
+
+  handleNewClick = (e) => {
+    console.log("New subfolder clicked");
+  }
+
+  handleChange = (e) => {
+    this.setState({...this.state, e.target.value});
+  }
+
   render() {
     // TODO: handle nexted arrays
     const folder_list = this.state.folders.map(item => {
       return (
-        <li value={item}>{item}</li>
+        <li className="folder" id={item}>
+          <span className="folder_name">{item}</span>
+          <button
+            className="new_subfolder"
+            onClick={this.handleNewClick}
+            title="New subfolder"
+          >⊕</button>
+          <button
+            className="delete_folder"
+            onClick={this.handleDelClick}
+            title="Delete folder"
+          >⊖</button>
+        </li>
       );
     });
     return (
-      <ul className="Folders">
-        {folder_list}
-      </ul>
+      <div className="folders">
+        <form onSubmit={this.handleSubmit} className="new_folder">
+          <input
+            type="text"
+            id="new_folder"
+            name="new_folder"
+            className="new_folder"
+            value={this.state.new_folder}
+            onChange={this.handleChange}
+          />
+          <button className="new_folder">New Folder</button>
+        </form>
+        <ul className="folder_list">
+          {folder_list}
+        </ul>
+      </div>
     );
   }
 }
