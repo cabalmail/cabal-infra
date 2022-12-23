cba6610c66a938b2dc395d5cb2511b02e46d510f
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-23 10:46:06 -0500

    Initial folders list

--- react/admin/src/Folders/index.js
+++ react/admin/src/Folders/index.js
@@ -1,8 +1,60 @@
 import React from 'react';
+import axios from 'axios';
+
+/**
+ * Fetches folders for current users and displays them
+ */
 
 class Folders extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      folders: []
+    };
+  }
+
+  componentDidMount() {
+    const response = this.getList();
+    response.then(data => {
+      this.setState({ ...this.state, folders: data.data });
+    }).catch(e => {
+      this.props.setMessage("Unable to fetch folders.", true);
+      console.log(e);
+    });
+  }
+
+  getList = (e) => {
+    const response = axios.get('/list_folders', {
+      params: {
+        host: this.props.host
+      },
+      baseURL: this.props.api_url,
+      headers: {
+        'Authorization': this.props.token
+      },
+      timeout: 10000
+    });
+    return response;
+  }
+
+  setFolder = (e) => {
+    e.preventDefault();
+    this.props.setFolder(e.target.value);
+  }
+
   render() {
-    return <div className="folders">Folders not yet implemented</div>
+    // TODO: handle nexted arrays
+    const folder_list = this.state.folders.map(item => {
+      return (
+        <li value={item}>{item}</li>
+      );
+    });
+    return (
+      <ul className="Folders">
+        {folder_list}
+      </ul>
+    );
   }
 }
 
