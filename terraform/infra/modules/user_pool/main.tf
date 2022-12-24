a87d686a62c84de727db2d3815131f9bc9e120a8
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-23 22:05:09 -0500

    Add osid attribute to user pool

--- terraform/infra/modules/user_pool/main.tf
+++ terraform/infra/modules/user_pool/main.tf
@@ -10,4 +10,14 @@ resource "aws_cognito_user_pool_client" "users" {
   name         = "cabal_admin_client"
   user_pool_id = aws_cognito_user_pool.users.id
   explicit_auth_flows = [ "USER_PASSWORD_AUTH" ]
+  schema {
+    name                     = "osid"
+    attribute_data_type      = "Number"
+    developer_only_attribute = false
+    mutable                  = false
+    required                 = true
+    number_attribute_constraints {
+      min_value = 2000
+    }
+  }
 }
