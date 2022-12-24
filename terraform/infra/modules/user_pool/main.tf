f7249c7ec649f7e099fdf17a82aebc8d08c78fc8
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-24 00:12:47 -0500

    Infra code for post confirmation hook

--- terraform/infra/modules/user_pool/main.tf
+++ terraform/infra/modules/user_pool/main.tf
@@ -2,6 +2,36 @@
 * Creates a Cognito User Pool for authentication against the management application and for authentication at the OS level (providing IMAP and SMTP authentication).
 */
 
+resource "aws_iam_role" "for_lambda" {
+  name               = "assign_osid"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "assign_osid" {
+  s3_bucket        = var.bucket_arn
+  s3_key           = "lambda/${var.name}.zip"
+  source_code_hash = data.aws_s3_object.lambda_function_hash.body
+  function_name    = "assign_osid"
+  role             = aws_iam_role.for_lambda.arn
+  handler          = "index.handler"
+  runtime          = "python3.9"
+  timeout          = 30
+}
+
 resource "aws_cognito_user_pool" "users" {
   name = "cabal"
   schema {
@@ -13,6 +43,9 @@ resource "aws_cognito_user_pool" "users" {
     number_attribute_constraints {
       min_value = 2000
     }
+  }
+  lambda_config {
+    post_confirmation = resource.aws_lambda_function.assign_osid.arn
   }
 }
 
