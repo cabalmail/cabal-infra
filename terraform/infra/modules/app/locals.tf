390022d9528095eec4a5db17965b31ea78fe100b
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-24 10:21:47 -0500

    Lambda layer for nodejs

--- terraform/infra/modules/app/locals.tf
+++ terraform/infra/modules/app/locals.tf
@@ -1,7 +1,10 @@
 locals {
   lambda_layers = {
     python = {
-      runtime   = "python3.9"
+      runtime = "python3.9"
+    },
+    nodejs = {
+      runtime = "nodejs14.x"
     }
   }
   lambdas = {
