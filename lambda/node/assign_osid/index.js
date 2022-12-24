455433436e37002b366b3b39a4d5ae33337de6e8
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-24 00:11:37 -0500

    First pass at post confirmation hook

---
+++ lambda/node/assign_osid/index.js
@@ -0,0 +1,52 @@
+const {
+  CognitoIdentityProviderClient,
+  ListUsersCommand,
+  AdminUpdateUserAttributesCommand
+} = require("@aws-sdk/client-cognito-identity-provider");
+
+const config = require('./config.js').config.cognitoConfig;
+
+exports.handler = (event, context, callback) => {
+  var user = event.request.userAttributes.username;
+  const client = new CognitoIdentityProviderClient({
+    region: config.region
+  });
+  const ListCommand = new ListUsersCommand({
+    UserPoolId: userPoolId
+  });
+  client.send(ListCommand)
+  .then(users => {
+    console.log(users);
+    var uid = 2000;
+    users.forEach(u => {
+      u.Attributes.forEach( a => {
+        if (a.Name == "custom:osid") {
+          if (a.Value > uid) {
+            uid = a.Value + 1;
+          }
+        }
+      });
+    });
+    const UpdateCommand = new AdminUpdateUserAttributesCommand({
+      UserPoolId: userPoolId,
+      UserAttributes: [
+        Name: "custom:osid",
+        Value: uid
+      ],
+      Username: user
+    });
+    client.send(UpdateCommand)
+    .then(data => {
+      console.log(data);
+      callback(null, event);
+    })
+    .catch(err => {
+      console.error(err);
+      callback(null, event);
+    });
+  })
+  .catch(err => {
+    console.error(err);
+    callback(null, event);
+  });
+}
