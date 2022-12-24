9aa00dc72e3160b8578035f4140bfc7d2bf5cfa0
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-24 11:17:52 -0500

    Map output from layers more usefully

--- terraform/infra/modules/lambda_layers/outputs.tf
+++ terraform/infra/modules/lambda_layers/outputs.tf
@@ -1,6 +1,6 @@
 output "layers" {
   value = {
-    "nodejs": aws_lambda_layer_version.layer["nodejs"].arn,
-    "python": aws_lambda_layer_version.layer["python"].arn
+    "nodejs" = aws_lambda_layer_version.layer["nodejs"].arn,
+    "python" = aws_lambda_layer_version.layer["python"].arn
   }
 }
