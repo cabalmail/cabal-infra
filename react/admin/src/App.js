1de2c0ac3ea6c70d0b9e7a16e78b5a5a2ba6bec4
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-27 21:50:24 -0500

    Hide Safari address bar

--- react/admin/src/App.js
+++ react/admin/src/App.js
@@ -73,6 +73,13 @@ class App extends React.Component {
       });
       UserPool = new CognitoUserPool(cognitoConfig.poolData);
     });
+    if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+      window.addEventListener("load",function() {
+        setTimeout(function() {
+          window.scrollTo(0, 1);
+        }, 1000);
+      });
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
