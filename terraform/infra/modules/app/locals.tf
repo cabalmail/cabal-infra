59e61af46e8283ae7f07e9b3c3839cf04554fcce
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2022-12-31 03:00:35 -0500

    Add config to api gateway for new_folder

--- terraform/infra/modules/app/locals.tf
+++ terraform/infra/modules/app/locals.tf
@@ -64,6 +64,14 @@ locals {
       cache     = true
       cache_ttl = 0
     },
+    new_folder = {
+      runtime   = "python3.9"
+      type      = "python"
+      method    = "PUT"
+      memory    = 128
+      cache     = true
+      cache_ttl = 0
+    },
     move_messages = {
       runtime   = "python3.9"
       type      = "python"
