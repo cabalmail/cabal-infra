e48e9e4d6bd0b4b2798816aeacbf7228bf0708c8
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-01-03 21:13:42 -0500

    data, not resource

--- terraform/dns/outputs.tf
+++ terraform/dns/outputs.tf
@@ -4,9 +4,9 @@ output "control_domain_name_servers" {
 
 output "github_response" {
   value = <<EO_RESP
-${resource.http.trigger_builds.status_code}
-${resource.http.trigger_builds.response_headers}
+${data.http.trigger_builds.status_code}
+${data.http.trigger_builds.response_headers}
 
-${resource.http.trigger_builds.response_body}
+${data.http.trigger_builds.response_body}
 EO_RESP
 }
