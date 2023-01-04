9f837908f6fdacee417c368d3c51753e99f7a34c
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-01-03 21:12:21 -0500

    Add github response to output

--- terraform/dns/outputs.tf
+++ terraform/dns/outputs.tf
@@ -1,3 +1,12 @@
 output "control_domain_name_servers" {
   value = aws_route53_zone.cabal_control_zone.name_servers
 }
+
+output "github_response" {
+  value = <<EO_RESP
+${resource.http.trigger_builds.status_code}
+${resource.http.trigger_builds.response_headers}
+
+${resource.http.trigger_builds.response_body}
+EO_RESP
+}
