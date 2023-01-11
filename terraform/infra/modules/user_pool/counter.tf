6f3caaf45a6bd5c6b052ac2b0bc7d0c29fe5159c
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-01-10 20:13:05 -0500

    ARN

--- terraform/infra/modules/user_pool/counter.tf
+++ terraform/infra/modules/user_pool/counter.tf
@@ -84,7 +84,7 @@ resource "aws_lambda_permission" "allow_cognito" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.assign_osid.function_name
   principal     = "cognito-idp.amazonaws.com"
-  source_arn    = aws_cognito_user_pool.users.id
+  source_arn    = aws_cognito_user_pool.users.arn
 }
 
 resource "aws_dynamodb_table" "counter" {
