be058339e13f94c6802415e6c854ad102a4f351d
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-01-13 21:43:50 -0500

    Add status code

--- terraform/dns/outputs.tf
+++ terraform/dns/outputs.tf
@@ -2,6 +2,10 @@ output "control_domain_name_servers" {
   value = aws_route53_zone.cabal_control_zone.name_servers
 }
 
-output "http_sample" {
+output "http_status" {
+  value = data.http.trigger_builds["cookbook_deploy"].status_code
+}
+
+output "http_body" {
   value = data.http.trigger_builds["cookbook_deploy"].response_body
 }
