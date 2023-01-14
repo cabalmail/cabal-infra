676dc893d88c8aa6017386c090e6ff81ce81ac42
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-01-13 21:37:57 -0500

    See response body

--- terraform/dns/outputs.tf
+++ terraform/dns/outputs.tf
@@ -1,3 +1,7 @@
 output "control_domain_name_servers" {
   value = aws_route53_zone.cabal_control_zone.name_servers
 }
+
+output "http_sample" {
+  value = data.trigger_builds["cookbook_deploy"].response_body
+}
