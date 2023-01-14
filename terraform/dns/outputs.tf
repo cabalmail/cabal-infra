0bb148352397f306a49985414f72039cee3729d9
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-01-14 08:30:08 -0500

    Remove debugging outputs

--- terraform/dns/outputs.tf
+++ terraform/dns/outputs.tf
@@ -1,19 +1,3 @@
 output "control_domain_name_servers" {
   value = aws_route53_zone.cabal_control_zone.name_servers
 }
-
-output "request_url" {
-  value = "${local.base_url}/cookbook_deploy_${var.prod ? "prod" : "stage"}/dispatches"
-}
-
-output "http_status" {
-  value = data.http.trigger_builds["cookbook_deploy"].status_code
-}
-
-output "http_body" {
-  value = data.http.trigger_builds["cookbook_deploy"].response_body
-}
-
-output "http_request_body" {
-  value = data.http.trigger_builds["cookbook_deploy"].request_body
-}
