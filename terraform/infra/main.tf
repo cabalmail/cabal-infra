a40c0ef0af3d3b887596632a4b7e70b05782e377
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-03-18 09:05:29 -0400

    Make environment tag a variable

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
   region = var.aws_region
   default_tags {
     tags = {
-      environment          = var.prod ? "production" : "non-production"
+      environment          = var.environment
       managed_by_terraform = "y"
       terraform_repo       = var.repo
     }
