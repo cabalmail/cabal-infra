ee1bf0881e2338021e595d91696bc8e5b6a8037d
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-04-13 20:55:45 -0400

    I don't like this bug after all

--- terraform/infra/modules/elb/main.tf
+++ terraform/infra/modules/elb/main.tf
@@ -7,7 +7,7 @@ resource "aws_lb" "elb" {
   internal                         = false #tfsec:ignore:aws-elb-alb-not-public
   load_balancer_type               = "network"
   subnets                          = var.public_subnet_ids
-  enable_cross_zone_load_balancing = false
+  enable_cross_zone_load_balancing = true
   tags                             = {
     Name = "cabal-nlb"
   }
