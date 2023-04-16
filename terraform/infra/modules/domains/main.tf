130f2aa87b6d31f91cbf42f3569d84d344fc33ac
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-04-15 20:18:29 -0400

    Create wildcard A record

--- terraform/infra/modules/domains/main.tf
+++ terraform/infra/modules/domains/main.tf
@@ -13,6 +13,7 @@ resource "aws_route53_record" "a" {
   for_each = toset(var.mail_domains)
   name     = "*.${each.key}"
   type     = "A"
+  ttl      = 3600
   zone_id  = aws_route53_zone.mail_dns[each.key].id
   records  = ["127.0.0.1"]
 }
