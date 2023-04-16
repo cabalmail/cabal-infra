357df7e9d4464c570e64beb0bf844db85cede8b3
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-04-15 20:32:57 -0400

    Revert A record wildcard

--- terraform/infra/modules/domains/main.tf
+++ terraform/infra/modules/domains/main.tf
@@ -8,12 +8,3 @@ resource "aws_route53_zone" "mail_dns" {
   comment       = "Domain for ${each.value} mail"
   force_destroy = true
 }
-
-resource "aws_route53_record" "a" {
-  for_each = toset(var.mail_domains)
-  name     = "*.${each.key}"
-  type     = "A"
-  ttl      = 3600
-  zone_id  = aws_route53_zone.mail_dns[each.key].id
-  records  = ["127.0.0.1"]
-}
