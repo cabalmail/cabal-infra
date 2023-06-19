de23b8c9d548af2981f8b520575d65dcf90a57ae
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-06-18 21:43:32 -0400

    Handle deep domain tree

--- lambda/api/python/fetch_bimi/function.py
+++ lambda/api/python/fetch_bimi/function.py
@@ -5,26 +5,26 @@ def handler(event, _context):
     '''Checks for the presence of a BIMI record and returns the image URL if found'''
     qs = event['queryStringParameters']
     sender_domain = qs['sender_domain']
-    try:
-        answer = dns.resolver.query(f'default._bimi.{sender_domain}', 'TXT')
-    except dns.resolver.NXDOMAIN:
-        return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "url": "/mask.png"
-            })
-        }
-    except dns.resolver.NoAnswer:
-        return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "url": "/mask.png"
-            })
-        }
+    sender_domain_parts = sender_domain.split(".")
+    for x in range(len(sender_domain_parts)):
+        domain = ".".join(sender_domain_parts[x:])
+        print(domain)
+        try:
+            answer = dns.resolver.query(f'default._bimi.{domain}', 'TXT')
+            return {
+                "statusCode": 200,
+                "body": json.dumps({
+                    "url": answer[0].__str__().split(";")[1].split("=")[1]
+                })
+            }
+        except dns.resolver.NXDOMAIN:
+            pass
+        except dns.resolver.NoAnswer:
+            pass
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "url": answer[0].__str__().split(";")[1].split("=")[1]
+            "url": "/mask.png"
         })
     }
