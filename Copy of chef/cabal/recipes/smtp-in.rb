5fe9fb70a7c7b46920c93994effb8acc3a433ae2
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-06-19 16:01:41 -0400

    Remove copy

--- Copy of chef/cabal/recipes/smtp-in.rb
+++
@@ -1,8 +0,0 @@
-include_recipe 'cabal::_common'
-include_recipe 'cabal::_smtp-common_sendmail'
-include_recipe 'cabal::_smtp-in_sendmail'
-include_recipe 'cabal::_common_users'
-
-port '25'
-port '465'
-port '587'
