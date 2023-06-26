57435b2e06bc62c44aa54cc4ee8ba1d302fa9cca
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-06-25 21:03:14 -0400

    Fix unmatched tags

--- react/admin/src/Email/ComposeOverlay/index.js
+++ react/admin/src/Email/ComposeOverlay/index.js
@@ -27,13 +27,13 @@ class ComposeOverlay extends React.Component {
           <label for="address-select-old">Use an existing address</label>
           <input type="radio" value="new" id="address-select-old" name="address-select" />
           <label for="address-select-new">Use an existing address</label>
-          <label for="address-to">To</legend>
+          <label for="address-to">To</label>
           <input type="text" id="address-to" name="address-to" />
-          <label for="address-cc">CC</legend>
+          <label for="address-cc">CC</label>
           <input type="text" id="address-cc" name="address-cc" />
-          <label for="address-bcc">BCC</legend>
+          <label for="address-bcc">BCC</label>
           <input type="text" id="address-bcc" name="address-cc" />
-          <label for="subject">Subject</legend>
+          <label for="subject">Subject</label>
           <input type="text" id="subject" name="subject" />
         </fieldset>
         <fieldset>
