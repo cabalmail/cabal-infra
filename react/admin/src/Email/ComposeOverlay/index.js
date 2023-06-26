e1e39fd386875ffbdd85bfcc30f9075943ff4ba4
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-06-25 21:01:01 -0400

    Fix syntax error

--- react/admin/src/Email/ComposeOverlay/index.js
+++ react/admin/src/Email/ComposeOverlay/index.js
@@ -23,18 +23,18 @@ class ComposeOverlay extends React.Component {
       <form className="compose-overlay" onSubmit={this.handleSubmit}>
         <fieldset>
           <legend>Envelope</legend>
-          <input type="radio" value="old" id="address-select-old" name="address-select/>
+          <input type="radio" value="old" id="address-select-old" name="address-select" />
           <label for="address-select-old">Use an existing address</label>
-          <input type="radio" value="new" id="address-select-old" name="address-select/>
+          <input type="radio" value="new" id="address-select-old" name="address-select" />
           <label for="address-select-new">Use an existing address</label>
           <label for="address-to">To</legend>
-          <input type="text" id="address-to" name="address-to"/>
+          <input type="text" id="address-to" name="address-to" />
           <label for="address-cc">CC</legend>
-          <input type="text" id="address-cc" name="address-cc"/>
+          <input type="text" id="address-cc" name="address-cc" />
           <label for="address-bcc">BCC</legend>
-          <input type="text" id="address-bcc" name="address-cc"/>
+          <input type="text" id="address-bcc" name="address-cc" />
           <label for="subject">Subject</legend>
-          <input type="text" id="subject" name="subject"/>
+          <input type="text" id="subject" name="subject" />
         </fieldset>
         <fieldset>
           <legend>Message</legend>
