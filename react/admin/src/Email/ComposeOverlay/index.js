f66eab44a3f3f009f9618ccea074556b0ae88cdf
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-06-25 20:58:16 -0400

    Skeleton of compose form

--- react/admin/src/Email/ComposeOverlay/index.js
+++ react/admin/src/Email/ComposeOverlay/index.js
@@ -1,9 +1,49 @@
 import React from 'react';
 
 class ComposeOverlay extends React.Component {
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    return false;
+  }
+
+  handleSend = (e) => {
+    e.preventDefault();
+    return false;
+  }
+
+  handleCancel = (e) => {
+    e.preventDefault();
+    // TODO: clear all inputs
+    this.props.hide();
+  }
+
   render() {
     return (
-      <div className="compose-overlay">Hello world</div>
+      <form className="compose-overlay" onSubmit={this.handleSubmit}>
+        <fieldset>
+          <legend>Envelope</legend>
+          <input type="radio" value="old" id="address-select-old" name="address-select/>
+          <label for="address-select-old">Use an existing address</label>
+          <input type="radio" value="new" id="address-select-old" name="address-select/>
+          <label for="address-select-new">Use an existing address</label>
+          <label for="address-to">To</legend>
+          <input type="text" id="address-to" name="address-to"/>
+          <label for="address-cc">CC</legend>
+          <input type="text" id="address-cc" name="address-cc"/>
+          <label for="address-bcc">BCC</legend>
+          <input type="text" id="address-bcc" name="address-cc"/>
+          <label for="subject">Subject</legend>
+          <input type="text" id="subject" name="subject"/>
+        </fieldset>
+        <fieldset>
+          <legend>Message</legend>
+        </fieldset>
+        <fieldset>
+          <button onClick={this.handleSend}>Send</button>
+          <button onClick={this.handleCancel}>Cancel</button>
+        </fieldset>
+      </form>
     );
   }
 }
