2269988a680acbeb6691f48330a10c860bc7542f
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-02 17:45:44 -0400

    Composer overlay styling

--- react/admin/src/Email/ComposeOverlay/Composer.js
+++ react/admin/src/Email/ComposeOverlay/Composer.js
@@ -33,20 +33,17 @@ class Composer extends React.Component {
       <div className="composer-wrapper">
         <label htmlFor="composer-text">Message Body</label>
         <div className="composer-toolbar">
-          <label className="composer-toolbar-style">
-            Style:
-            <select id="composer-toolbar-style-select" className="composer-toolbar-style-select">
-              <option value="body-text">Body Text</option>
-              <option value="h1">Header Level 1</option>
-              <option value="h2">Header Level 2</option>
-              <option value="h2">Header Level 3</option>
-              <option value="h2">Header Level 4</option>
-              <option value="h2">Header Level 5</option>
-              <option value="h2">Header Level 6</option>
-              <option value="block-quote">Block Quote</option>
-              <option value="pre">Monospace</option>
-            </select>
-          </label>
+        <select id="composer-toolbar-style-select" className="composer-toolbar-style-select">
+            <option value="body-text">Body Text</option>
+            <option value="h1">Header Level 1</option>
+            <option value="h2">Header Level 2</option>
+            <option value="h2">Header Level 3</option>
+            <option value="h2">Header Level 4</option>
+            <option value="h2">Header Level 5</option>
+            <option value="h2">Header Level 6</option>
+            <option value="block-quote">Block Quote</option>
+            <option value="pre">Monospace</option>
+          </select>
           <button className="composer-toolbar-button composer-toolbar-bold">B</button>
           <button className="composer-toolbar-button composer-toolbar-italic">I</button>
           <button className="composer-toolbar-button composer-toolbar-underline">U</button>
