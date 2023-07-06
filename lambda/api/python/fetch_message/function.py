4efa89d5467dc0681829e9c86533016d0ae4aad6
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-06 15:58:00 -0400

    Decode with given charset

--- lambda/api/python/fetch_message/function.py
+++ lambda/api/python/fetch_message/function.py
@@ -10,28 +10,37 @@ def handler(event, _context):
     message = get_message(qs['host'], user, qs['folder'], int(qs['id']))
     body_plain = ""
     body_html = ""
+    body_html_charset = "utf8"
+    body_plain_charset = "utf8"
     if message.is_multipart():
         for part in message.walk():
             ct = part.get_content_type()
+            ch = part.get_content_charset()
             cd = str(part.get('Content-Disposition'))
             if ct == 'text/plain' and 'attachment' not in cd:
                 body_plain = part.get_payload(decode=True)
+                body_plain_charset = ch
             if ct == 'text/html' and 'attachment' not in cd:
                 body_html = part.get_payload(decode=True)
+                body_html_charset = ch
     else:
         ct = message.get_content_type()
+        ch = message.get_content_charset()
         if ct == 'text/plain':
             body_plain = message.get_payload(decode=True)
+            body_plain_charset = ch
         if ct == 'text/html':
             body_html = message.get_payload(decode=True)
+            body_html_charset = ch
 
     try:
-        body_html_decoded = body_html.decode()
+        body_html_decoded = body_html.decode(body_html_charset)
     except:
+        print("Woopsy")
         body_html_decoded = body_html.__str__()
 
     try:
-        body_plain_decoded = body_plain.decode()
+        body_plain_decoded = body_plain.decode(body_plain_charset)
     except:
         body_plain_decoded = body_plain.__str__()
 
