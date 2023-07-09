7cfef72a99b61af2fc45a0e95916173ec4ea776b
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-09 16:42:36 -0400

    Try to scroll to insertion point

--- react/admin/src/Email/ComposeOverlay/Composer.js
+++ react/admin/src/Email/ComposeOverlay/Composer.js
@@ -97,6 +97,7 @@ class Composer extends React.Component {
         || prevState.cursorEnd !== this.state.cursorEnd) {
       setTimeout(() => {
         const ta = document.getElementById("composer-text");
+        window.getSelection().collapse(body, 0);
         ta.blur();
         ta.focus();
         ta.selectionStart = this.state.cursorStart;
