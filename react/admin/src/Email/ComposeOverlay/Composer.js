7a853feef5e9ec9a7eb9d41345b63e94b13c9622
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-10 17:59:03 -0400

    Oh. Yeah.

--- react/admin/src/Email/ComposeOverlay/Composer.js
+++ react/admin/src/Email/ComposeOverlay/Composer.js
@@ -17,7 +17,6 @@ class Composer extends React.Component {
 
   constructor(props) {
     super(props);
-    this.scroll = 0;
     this.state = JSON.parse(localStorage.getItem(STATE_KEY)) || {
       markdown: "",
       history: [""],
@@ -36,23 +35,6 @@ class Composer extends React.Component {
     }
     super.setState(state);
   }
-
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.cursorStart !== this.state.cursorStart
-  //       || prevState.cursorEnd !== this.state.cursorEnd) {
-  //     setTimeout(() => {
-  //       const ta = document.getElementById("composer-text");
-  //       ta.selectionStart = this.state.cursorStart;
-  //       ta.selectionEnd = this.state.cursorEnd;
-  //       if (ta.value.length < this.state.cursorEnd + 60) {
-  //         // Cursor is near the bottom of the textarea
-  //         ta.scrollTop = 999999999;
-  //       } else {
-  //         ta.scrollTop =  this.scroll;
-  //       }
-  //     }, 20);
-  //   }
-  // }
 
   historyPush(md, cs, ce) {
     var history = this.state.history.slice(0, this.state.history_index + 1);
@@ -115,7 +97,6 @@ class Composer extends React.Component {
     var end = e.target.selectionEnd;
     var newCursorStart = start;
     var newCursorEnd = end;
-    this.scroll = e.target.scrollTop;
     switch (e.keyCode) {
       // TODO: 
       // - delete key
