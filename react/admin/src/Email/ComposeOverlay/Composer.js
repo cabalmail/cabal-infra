f757187353d6d77a493b695e053a8b6e17410168
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-10 12:11:47 -0400

    Move Preview to own component

--- react/admin/src/Email/ComposeOverlay/Composer.js
+++ react/admin/src/Email/ComposeOverlay/Composer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './ComposeOverlay.css';
-import DOMPurify from 'dompurify';
+import Preview from './Preview';
 const STATE_KEY = 'composer-state';
 
 class Composer extends React.Component {
@@ -340,41 +340,6 @@ class Composer extends React.Component {
     this.setState({...this.state, preview: false});
   }
 
-  renderPreview() {
-    var html = DOMPurify.sanitize(this.state.markdown);
-    // bold
-    html = html.replace(/__(.*?)__/g, "<strong>$1</strong>");
-    // italic
-    html = html.replace(/_(.*?)_/g, "<em>$1</em>");
-    // strikethrough
-    html = html.replace(/~~(.*?)~~/g, "<span style=\"text-decoration:line-through\">$1</span>");
-    // link
-    html = html.replace(/\[(.*?)\]\((.*?)\)/g, "<a href=\"$2\">$1</a>");
-    // paragraphs
-    html = `<p>${html}</p>`;
-    html = html.replace(/\n/g,"</p><p>");
-    // headers
-    html = html.replace(/<p>###### *(.*?)<\/p>/g,"<h6>$1</h6>");
-    html = html.replace(/<p>##### *(.*?)<\/p>/g,"<h5>$1</h5>");
-    html = html.replace(/<p>#### *(.*?)<\/p>/g,"<h4>$1</h4>");
-    html = html.replace(/<p>### *(.*?)<\/p>/g,"<h3>$1</h3>");
-    html = html.replace(/<p>## *(.*?)<\/p>/g,"<h2>$1</h2>");
-    html = html.replace(/<p># *(.*?)<\/p>/g,"<h1>$1</h1>");
-    // block quotes
-    html = html.replace(/<p>> *?(.*?)<\/p>/g,"<h1>$1</h1>");
-    // monospace
-    html = html.replace(/<p> {4}(.*?)<\/p>/g,"<pre>$1</pre>");
-    html = html.replace(/<\/pre><pre>/g,"\n");
-    html = html.split("<pre>").map(s => {
-      let str = s;
-      str = str.replaceAll(/&(?=[^]*?<\/pre>)/g, "&amp;");
-      str = str.replaceAll(/<(?=[^]*?<\/pre>)/g, "&lt;");
-      str = str.replaceAll(/>(?=[^]*?<\/pre>)/g, "&gt;");
-      return str;
-    }).join("<pre>");
-    return <div dangerouslySetInnerHTML={{__html: html}}></div>;
-  }
-
   render() {
     const previewClass = this.state.preview
                        ? "composer-wrapper composer-preview-preview"
@@ -429,7 +394,7 @@ class Composer extends React.Component {
           />
         </div>
         <div id="composer-preview">
-          {this.renderPreview()}
+          <Preview markdown={this.state.markdown} />
         </div>
         <div className="composer-preview-toggle">
           <button
---
+++ react/admin/src/Email/ComposeOverlay/Preview.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './ComposeOverlay.css';
+import DOMPurify from 'dompurify';
+// const STATE_KEY = 'preview-state';
+
+class Preview extends React.Component {
+  render() {
+    var html = DOMPurify.sanitize(this.props.markdown);
+    // bold
+    html = html.replace(/__(.*?)__/g, "<strong>$1</strong>");
+    // italic
+    html = html.replace(/_(.*?)_/g, "<em>$1</em>");
+    // strikethrough
+    html = html.replace(/~~(.*?)~~/g, "<span style=\"text-decoration:line-through\">$1</span>");
+    // link
+    html = html.replace(/\[(.*?)\]\((.*?)\)/g, "<a href=\"$2\">$1</a>");
+    // paragraphs
+    html = `<p>${html}</p>`;
+    html = html.replace(/\n/g,"</p><p>");
+    // headers
+    html = html.replace(/<p>###### *(.*?)<\/p>/g,"<h6>$1</h6>");
+    html = html.replace(/<p>##### *(.*?)<\/p>/g,"<h5>$1</h5>");
+    html = html.replace(/<p>#### *(.*?)<\/p>/g,"<h4>$1</h4>");
+    html = html.replace(/<p>### *(.*?)<\/p>/g,"<h3>$1</h3>");
+    html = html.replace(/<p>## *(.*?)<\/p>/g,"<h2>$1</h2>");
+    html = html.replace(/<p># *(.*?)<\/p>/g,"<h1>$1</h1>");
+    // block quotes
+    html = html.replace(/<p>> *?(.*?)<\/p>/g,"<h1>$1</h1>");
+    // monospace
+    html = html.replace(/<p> {4}(.*?)<\/p>/g,"<pre>$1</pre>");
+    html = html.replace(/<\/pre><pre>/g,"\n");
+    html = html.split("<pre>").map(s => {
+      return s.replaceAll(/&(?=[^]*?<\/pre>)/g, "&amp;");
+              .replaceAll(/<(?=[^]*?<\/pre>)/g, "&lt;");
+              .replaceAll(/>(?=[^]*?<\/pre>)/g, "&gt;");
+    }).join("<pre>");
+    return <div dangerouslySetInnerHTML={{__html: html}}></div>;
+  }
+}
+
+export default Preview;
