cebd600327a2b4d78a34cba7b0ab58e0eeb7146f
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-16 10:59:53 -0400

    First attempt at node api ci/cd

---
+++ .github/scripts/build-api-node.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+FUNCTIONS=$(echo *)
+AWS_S3_BUCKET="admin.$(aws ssm get-parameter --name '/cabal/control_domain_zone_name' --profile deploy_lambda | jq -r '.Parameter.Value')"
+for FUNC in $FUNCTIONS ; do
+  pushd $FUNC
+  if test -f requirements.txt; then
+    mkdir nodejs
+    pushd nodejs
+    npm init --yes
+    cat ../requirements.txt | xargs npm install --save 
+    popd
+  fi
+  zip -r ../$FUNC.zip .
+  popd
+  openssl dgst -sha256 -binary "$FUNC.zip" | openssl enc -base64 | tr -d "\n" > "$FUNC.zip.base64sha256"
+  aws s3 cp "$FUNC.zip.base64sha256" "s3://${AWS_S3_BUCKET}/lambda/$FUNC.zip.base64sha256" --profile deploy_lambda --no-progress --acl private --content-type text/plain
+  aws s3 cp "${FUNC}.zip" "s3://${AWS_S3_BUCKET}/lambda/${FUNC}.zip" --profile deploy_lambda --no-progress --acl private
+done
