7cb99acf01b772bf7ea5cb0188cb8a223a3033fb
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-18 19:52:33 -0400

    Style recipient lists

--- react/admin/src/Email/ComposeOverlay/index.js
+++ react/admin/src/Email/ComposeOverlay/index.js
@@ -320,8 +320,11 @@ class ComposeOverlay extends React.Component {
           >+ BCC</button>
         </div>
         <div className="recipients">
+          <label className={this.state.To.length ? "show" : "hide"}>To</label>
           <ul className="recipient-list" id="to-list">{to_list}</ul>
+          <label className={this.state.CC.length ? "show" : "hide"}>CC</label>
           <ul className="recipient-list" id="cc-list">{cc_list}</ul>
+          <label className={this.state.BCC.length ? "show" : "hide"}>BCC</label>
           <ul className="recipient-list" id="bcc-list">{bcc_list}</ul>
         </div>
         <label htmlFor="subject">Subject</label>
