bfc3be25f8dc2bf152f0bc65b3b47c1511420975
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-21 13:43:21 -0400

    Initial work on Lambda function to support send

---
+++ lambda/api/python/send/function.py
@@ -0,0 +1,34 @@
+'''Sends an email message'''
+import json
+from helper import get_imap_client # pylint: disable=import-error
+from email.message import EmailMessage # pylint: disable=import-error
+     # Flow:
+     # - place in outbox
+     # - send via SMTP
+     # - on succes, move from outbox to Sent
+    
+def handler(event, _context):
+    '''Sends an email message'''
+    body = json.loads(event['body'])
+    user = event['requestContext']['authorizer']['claims']['cognito:username']
+    client = get_imap_client(body['host'], user, 'INBOX')
+    msg = EmailMessage()
+    msg['Subject'] = body['subject']
+    msg['From'] = body['sender']
+    msg['To'] = ','.join(body['to_list'])
+    msg['Cc'] = ','.join(body['cc_list'])
+    msg['Bcc'] = ','.join(body['bcc_list'])
+    msg.set_content("This is a Multi-part message.")
+    msg.set_alternative(body['body'], subtype='html')
+    try:
+        client.create_folder('Outbox')
+    except: # pylint: disable=bare-except
+        pass
+    client.append('Outbox',msg)
+    client.logout()
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "status": "submitted"
+        })
+    }
