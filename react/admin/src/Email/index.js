66819aefc088331058af0c15d7ac3f2580d1bcee
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-24 15:19:43 -0400

    Try unmount

--- react/admin/src/Email/index.js
+++ react/admin/src/Email/index.js
@@ -18,7 +18,7 @@ class Email extends React.Component {
     };
   }
 
-  componentDidMount() {
+  componentWillUnmount() {
     // hide the overlay when switching to this component,
     // even if saved state wants to show it
     this.setState({...this.state, overlayVisible: false, composeVisible: false, envelope: {}});
