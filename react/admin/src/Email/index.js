afe3c4600441cc97aba43cd5962563cff126e9ea
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-24 15:15:25 -0400

    Fix envelope not loading

--- react/admin/src/Email/index.js
+++ react/admin/src/Email/index.js
@@ -21,7 +21,7 @@ class Email extends React.Component {
   componentDidMount() {
     // hide the overlay when switching to this component,
     // even if saved state wants to show it
-    this.setState({...this.state, overlayVisible: false, composeVisible: false, this.envelope: {}});
+    this.setState({...this.state, overlayVisible: false, composeVisible: false, envelope: {}});
   }
   setState(state) {
     try {
