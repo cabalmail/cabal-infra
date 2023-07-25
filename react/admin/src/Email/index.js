ec44c90172666e3d6f7cfa0050d891b80d5d4aa7
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-25 14:45:33 -0400

    Debugging subject

--- react/admin/src/Email/index.js
+++ react/admin/src/Email/index.js
@@ -64,6 +64,7 @@ class Email extends React.Component {
 
   reply = (recipient, body, envelope, other_headers) => {
     const subject = envelope.subject.replace(/^(re|fwd):?\s(.$)$/i, "Re: $2");
+    console.log(`Converting ${envelope.subject} to ${subject}`);
     this.setState({
       ...this.state,
       new_envelope: envelope,
