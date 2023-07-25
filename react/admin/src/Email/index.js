3e7ac368cae17cdf9e077c9fbef02f8b3a152f75
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-25 17:55:35 -0400

    Only return body content

--- react/admin/src/Email/index.js
+++ react/admin/src/Email/index.js
@@ -69,7 +69,8 @@ class Email extends React.Component {
       `<div>To: ${envelope.to.join("; ")}</div>` +
       `<div>Date: ${envelope.date}</div>` +
       `<div>Subject: ${envelope.subject}</div><div><p>&nbsp;</p></div>` +
-      body.replace(/<!--.*?-->/gm, "").replace(/&lt;!--.*?--&gt;/gm, "");
+      body.replace(/<!--.*?-->/gm, "").replace(/&lt;!--.*?--&gt;/gm, "")
+        .replace(/.*<body>/m, "").replace(<\/body>.*/m, "");
     this.setState({
       ...this.state,
       new_envelope: envelope,
