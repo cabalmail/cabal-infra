b9190358d207fde27b3d995f16db17098515b707
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-27 14:24:25 -0400

    Circumvent convertFromHTMLToContentBlocks regex

--- react/admin/src/Email/index.js
+++ react/admin/src/Email/index.js
@@ -64,11 +64,11 @@ class Email extends React.Component {
 
   reply = (recipient, body, envelope, other_headers) => {
     const subject = envelope.subject.replace(/^(re:?\s|fwd:?\s)?(.*)$/i, "Re: $2");
-    const extended_body = '<div style="min-height: 3em; border-bottom: 3px groove #888">&nbsp;</div>' +
+    const extended_body = '<div style="min-height: 3em; border-bottom: 3px groove #888">&#160;</div>' +
       `<div style="font-weight: bold;">From: ${envelope.from[0]}</div>` +
       `<div style="font-weight: bold;">To: ${envelope.to.join("; ")}</div>` +
       `<div style="font-weight: bold;">Date: ${envelope.date}</div>` +
-      `<div style="font-weight: bold;">Subject: ${envelope.subject}</div><div><p>&nbsp;</p></div>` +
+      `<div style="font-weight: bold;">Subject: ${envelope.subject}</div><div><p>&#160;</p></div>` +
       body.replace(/<!--[\s\S]*?-->/gm, "").replace(/&lt;!--[\s\S]*?--&gt;/gm, "")
         .replace(/[\s\S]*<body>/m, "").replace(/<\/body>[\s\S]*/m, "");
     this.setState({
