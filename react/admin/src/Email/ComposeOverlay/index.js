296ac204aa56d7de444348cdd78ae754bbd5ef89
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-07-27 17:00:56 -0400

    Revert

--- react/admin/src/Email/ComposeOverlay/index.js
+++ react/admin/src/Email/ComposeOverlay/index.js
@@ -76,9 +76,6 @@ class ComposeOverlay extends React.Component {
       To: this.props.envelope.from,
       Subject: this.props.subject
     });
-    if (this.props.type === "reply") {
-      this.refs.editor.focus();
-    }
     this.api.getAddresses().then(data => {
       try {
         localStorage.setItem(ADDRESS_LIST, JSON.stringify(data));
@@ -422,7 +419,6 @@ class ComposeOverlay extends React.Component {
           value={this.state.Subject}
         />
         <Editor
-          ref="editor"
           editorState={editorState}
           toolbarClassName="wysiwyg-toolbar"
           wrapperClassName="wysiwyg-wrapper"
