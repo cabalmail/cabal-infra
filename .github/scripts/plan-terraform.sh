5ed226d20fee2ed332fdfe85c12d9bc241d517a8
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-08-05 15:52:39 -0400

    Flailing

--- .github/scripts/plan-terraform.sh
+++ .github/scripts/plan-terraform.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
+set +e
 terraform plan -lock-timeout=30m -detailed-exitcode
 EXIT_CODE=$?
 echo ">$EXIT_CODE<"
 echo "exit_code=$EXIT_CODE" >> "$GITHUB_OUTPUT"
 cat $GITHUB_OUTPUT
-cat tmp.json | jq '.'
-case $EXIT_CODE in
-  '2')
-    exit 0
-    ;;
-  '1')
-    exit 1
-    ;;
-  '0')
-    exit 0
-    ;;
-esac
+if [[ $EXIT_CODE -eq 2 ]]; then
+  exit 0
+else
+  exit $EXIT_CODE
+fi
