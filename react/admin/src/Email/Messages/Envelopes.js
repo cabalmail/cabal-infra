3f7307b21dd5c098ac857d8266ed413fa98b980b
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-08-07 17:23:48 -0400

    Bug fix

--- react/admin/src/Email/Messages/Envelopes.js
+++ react/admin/src/Email/Messages/Envelopes.js
@@ -58,59 +58,22 @@ class Envelopes extends React.Component {
   archive = (id) => {
     this.props.archive(id);
   }
-  // loadList() {
-  //   const num_ids = this.state.message_ids.length;
-  //   var pages = [];
-  //   for (var i = 0; i < num_ids; i+=PAGE_SIZE) {
-  //     pages.push(
-  //       <LazyLoad offset={150} overflow={true}>
-  //         <Envelopes
-  //           message_ids={this.state.message_ids.slice(i, i+PAGE_SIZE)}
-  //           folder={this.props.folder}
-  //           host={this.props.host}
-  //           token={this.props.token}
-  //           api_url={this.props.api_url}
-  //           selected_messages={this.state.selected_messages}
-  //           showOverlay={this.props.showOverlay}
-  //           handleCheck={this.handleCheck}
-  //           handleSelect={this.handleSelect}
-  //           setMessage={this.props.setMessage}
-  //           markUnread={this.markUnread}
-  //           markRead={this.markRead}
-  //           archive={this.archive}
-  //         />
-  //       </LazyLoad>
-  //     );
-  //   }
-  //   return pages;
-  // }
 
   render() {
     var i = 0;
-    const message_list = this.props.message_ids.map(id => {
-      if (id.toString() in this.state.envelopes) {
-        return (
-          <Envelope
-            handleClick={this.handleClick}
-            handleCheck={this.handleCheck}
-            archive={this.archive}
-            markRead={this.markRead}
-            markUnread={this.markUnread}
-            envelope={this.state.envelopes[id]}
-            checked={this.props.selected_messages.includes(id)}
-            id={id}
-            index={i}
-          />
-        );
-      }
+    const message_list = this.state.envelopes.map(e => {
       return (
-        <div className="message-row loading" key={id}>
-          <div className="message-line-1">
-            <div className="message-field message-from">&nbsp;</div>
-            <div className="message-field message-date">&nbsp;</div>
-          </div>
-          <div className="message-field message-subject">&nbsp;</div>
-        </div>
+        <Envelope
+          handleClick={this.handleClick}
+          handleCheck={this.handleCheck}
+          archive={this.archive}
+          markRead={this.markRead}
+          markUnread={this.markUnread}
+          envelope={e}
+          checked={this.props.selected_messages.includes(e.id)}
+          id={e.id}
+          index={i}
+        />
       );
     });
     return (
