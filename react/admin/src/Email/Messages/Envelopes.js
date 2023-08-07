ee3b2f9b28b8707780d5ae23c2db725ace64dd75
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-08-07 15:55:43 -0400

    Try spacing out envelope retrieval

--- react/admin/src/Email/Messages/Envelopes.js
+++ react/admin/src/Email/Messages/Envelopes.js
@@ -3,6 +3,7 @@ import { SwipeableList } from 'react-swipeable-list';
 import 'react-swipeable-list/dist/styles.css';
 import Envelope from './Envelope';
 import ApiClient from '../../ApiClient';
+import { PAGE_SIZE } from '../../constants';
 import './Envelopes.css';
 
 class Envelopes extends React.Component {
@@ -18,18 +19,35 @@ class Envelopes extends React.Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (this.props.message_ids !== prevProps.message_ids) {
-      const response = this.api.getEnvelopes(
-        this.props.folder,
-        this.props.message_ids
-      );
-      response.then(data => {
-        this.setState({
-          ...this.state,
-          envelopes: data.data.envelopes
-        });
-      }).catch( e => {
-        console.log(e);
-      });
+
+
+
+      const num_ids = this.state.message_ids.length;
+      for (var i = 0; i < num_ids; i+=PAGE_SIZE) {
+
+        setInterval(() => {
+
+          const response = this.api.getEnvelopes(
+            this.props.folder,
+            this.props.message_ids.slice(i, i+PAGE_SIZE)
+          );
+          response.then(data => {
+            let envelopes = this.state.envelopes.slice();
+            envelopes.concat(data.data.envelopes);
+            this.setState({
+              ...this.state,
+              envelopes: envelopes
+            });
+          }).catch( e => {
+            console.log(e);
+          });
+
+        }, 1000 * (i/PAGE_SIZE));
+
+      }
+
+
+
     }
   }
 
@@ -54,8 +72,35 @@ class Envelopes extends React.Component {
   archive = (id) => {
     this.props.archive(id);
   }
+  // loadList() {
+  //   const num_ids = this.state.message_ids.length;
+  //   var pages = [];
+  //   for (var i = 0; i < num_ids; i+=PAGE_SIZE) {
+  //     pages.push(
+  //       <LazyLoad offset={150} overflow={true}>
+  //         <Envelopes
+  //           message_ids={this.state.message_ids.slice(i, i+PAGE_SIZE)}
+  //           folder={this.props.folder}
+  //           host={this.props.host}
+  //           token={this.props.token}
+  //           api_url={this.props.api_url}
+  //           selected_messages={this.state.selected_messages}
+  //           showOverlay={this.props.showOverlay}
+  //           handleCheck={this.handleCheck}
+  //           handleSelect={this.handleSelect}
+  //           setMessage={this.props.setMessage}
+  //           markUnread={this.markUnread}
+  //           markRead={this.markRead}
+  //           archive={this.archive}
+  //         />
+  //       </LazyLoad>
+  //     );
+  //   }
+  //   return pages;
+  // }
 
   render() {
+    var i = 0;
     const message_list = this.props.message_ids.map(id => {
       if (id.toString() in this.state.envelopes) {
         return (
@@ -68,6 +113,7 @@ class Envelopes extends React.Component {
             envelope={this.state.envelopes[id]}
             checked={this.props.selected_messages.includes(id)}
             id={id}
+            index={i}
           />
         );
       }
