bbf3f2a7c454b9693f442f1551a294801a6f2559
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2023-09-01 06:41:17 -0400

    Permanently remove commented-out code

--- react/admin/src/Email/Messages/Envelopes.js
+++ react/admin/src/Email/Messages/Envelopes.js
@@ -48,7 +48,6 @@ class Envelopes extends React.Component {
 
   doUpdate() {
     const num_ids = this.props.message_ids.length;
-    // this.setState({...this.state, envelopes: {}, pages: []});
     for (var i = 0; i < num_ids; i+=PAGE_SIZE) {
       let ids = this.props.message_ids.slice(i, i+PAGE_SIZE);
       let page = Math.floor(i/PAGE_SIZE);
