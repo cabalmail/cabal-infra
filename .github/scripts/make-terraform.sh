1be830cb63b8d948949f91fa7af62f104c5adafe
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2024-08-14 15:42:08 -0400

    Fix key name for S3 backend

--- .github/scripts/make-terraform.sh
+++ .github/scripts/make-terraform.sh
@@ -3,7 +3,7 @@ cat << EO_TF > terraform/infra/versions.tf
 terraform {
   backend "s3" {
     bucket = "cabal-tf-backend"
-    key    = "$TF_ENVIRONMENT/"
+    key    = "$TF_ENVIRONMENT"
     region = "$TF_VAR_AWS_REGION"
   }
 }
