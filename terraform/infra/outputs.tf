1a4afa8b6e59bdf4608c4c9c1462757760ef83d9
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2024-08-14 11:50:52 -0400

    Refactoring

--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -1,13 +1,13 @@
 output "IMPORTANT" {
-  value       = [
+  value = [
     "You must get permission from AWS to relay mail through the below IP addresses. See the section on Port 25 in docs/setup.md.",
     "You must update your domain registrations with the name servers from the below domains. See the section on Nameservers in docs/setup.md"
   ]
-	description = "Instructions for post-automation steps."
+  description = "Instructions for post-automation steps."
 }
 
 output "relay_ips" {
-  value       = {
+  value = {
     addresses = module.vpc.relay_ips
     domain    = "smtp.${var.control_domain}"
   }
