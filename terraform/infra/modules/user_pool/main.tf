e7a60f1ae1f39d1e727b3849877020401e0c3a7d
Chris Carr <b6595aec-f881-4819@9b9a-1d626ea6a1b5.ccarr.com>
2026-04-22 13:59:39 -0400

    Remove deletion protection from phone number

--- terraform/infra/modules/user_pool/main.tf
+++ terraform/infra/modules/user_pool/main.tf
@@ -35,11 +35,10 @@ resource "aws_cognito_user_pool" "users" {
 }
 
 resource "aws_pinpointsmsvoicev2_phone_number" "sms" {
-  iso_country_code          = "US"
-  message_type              = "TRANSACTIONAL"
-  number_capabilities       = ["SMS"]
-  number_type               = "TOLL_FREE"
-  deletion_protection_enabled = false
+  iso_country_code    = "US"
+  message_type        = "TRANSACTIONAL"
+  number_capabilities = ["SMS"]
+  number_type         = "TOLL_FREE"
 
   timeouts {
     create = "1m"
